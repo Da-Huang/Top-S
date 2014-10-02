8aff5e4400a47a90e9e98f2409978f87a0e1dbec
Da-Huang <dhuang.cn@gmail.com>
2014-10-02 10:39:24 +0800

    Partition List

---
+++ Leet/Partition List.cpp
@@ -0,0 +1,21 @@
+ListNode *partition(ListNode *head, int x) {
+  ListNode HEAD(0);
+  HEAD.next = head;
+  ListNode NEWHEAD(0);
+  ListNode *tail = &NEWHEAD;
+  ListNode *ptr = &HEAD;
+  while ( ptr->next ) {
+    if ( ptr->next->val < x ) {
+      ListNode *node = ptr->next;
+      ptr->next = node->next;
+      tail->next = node;
+      tail = node;
+
+    } else {
+      ptr = ptr->next;
+    } 
+  }
+  tail->next = HEAD.next;
+  return NEWHEAD.next;
+}
+
