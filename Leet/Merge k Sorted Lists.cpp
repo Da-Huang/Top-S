b3e277a7287a24b8dbb8fc0acd865a5e7f93084f
Da Huang <dhuang.cn@gmail.com>
2014-10-08 06:49:51 +0800

    Merge k Sorted Lists v2

--- Leet/Merge k Sorted Lists.cpp
+++ Leet/Merge k Sorted Lists.cpp
@@ -1,35 +1,30 @@
 // Redo
 ListNode *mergeKLists(vector<ListNode *> &lists) {
-  ListNode HEAD(0);
-  ListNode *tail = &HEAD;
   const int N = lists.size();
-  ListNode *begin[N];
-  for (int i = 0; i < N; i ++) begin[i] = lists[i];
-
-  auto lessNode = [&begin] (int i, int j) { // should be greater
-    return begin[i]->val > begin[j]->val;
-  };
   int pq[N];
   int pqLen = 0;
+  auto lessNode = [&lists] (int i, int j) {
+    return lists[i]->val > lists[j]->val;
+  };
+  ListNode HEAD(0);
+  ListNode *tail = &HEAD;
   for (int i = 0; i < N; i ++) {
-    if ( begin[i] ) {
+    if ( lists[i] ) {
       pq[pqLen ++] = i;
       push_heap(pq, pq + pqLen, lessNode);
     }
   }
   while ( pqLen ) {
     pop_heap(pq, pq + pqLen, lessNode);
-    pqLen --;
-    ListNode *node = begin[pq[pqLen]];
-    begin[pq[pqLen]] = node->next;
-    tail->next = node;
-    node->next = NULL;
-    tail = node;
-
-    if ( begin[pq[pqLen]] ) {
-      pqLen ++;
+    int i = pq[-- pqLen];
+    ListNode *node = lists[i];
+    lists[i] = lists[i]->next;
+    if ( lists[i] ) {
+      pq[pqLen ++] = i;
       push_heap(pq, pq + pqLen, lessNode);
     }
+    tail->next = node;
+    tail = node;
   }
   return HEAD.next;
 }
