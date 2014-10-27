2c8f2eaf16648cb14dc3cfb24e2bac6b95a2d408
Da-Huang <dhuang.cn@gmail.com>
2014-10-27 10:01:55 +0800

    Wildcard Matching v2

--- Leet/Wildcard Matching.cpp
+++ Leet/Wildcard Matching.cpp
@@ -1,43 +1,34 @@
 // Redo
 bool isMatch(const char *s, const char *p) {
   const int N = strlen(s);
-  const int M = strlen(p);
-  int i = 0, j = 0;
+  if ( *p == 0 ) return N == 0;
   auto equalC = [] (char c1, char c2) {
     return c1 == c2 || c1 == '?' || c2 == '?';
   };
-  while ( s[i] && p[i] && p[i] != '*' ) {
-    if ( !equalC(s[i], p[j]) ) return false;
-    i ++; j ++;
-  }
-  if ( p[j] == 0 && s[i] == 0 ) return true;
-  if ( p[j] == 0 ) return false;
 
-  // p[j] == '*'
-  while ( p[j] == '*' ) j ++;
-  if ( p[j] == 0 ) return true;
-  if ( s[i] == 0 ) return false;
-
-  while ( p[j] ) {
-    int endJ = j + 1;
-    while ( p[endJ] && p[endJ] != '*' ) endJ ++;
-    int endJ2 = endJ;
-    while ( p[endJ2] == '*' ) endJ2 ++;
-
-    if ( p[endJ] ) {
-      auto it = search(s + i, s + N, p + j, p + endJ, equalC);
-      if ( it == s + N ) return false;
-      i = distance(s, it) + endJ - j;
-      j = endJ2;
-      if ( p[j] == 0 ) return true;
-
-    } else {
-      auto it = find_end(s + i, s + N, p + j, p + endJ, equalC);
-      if ( it == s + N ) return false;
-      i = distance(s, it) + endJ - j;
-      return i == N;
+  int i = 0;
+  int begin = 0;
+  while ( p[begin] ) {
+    if ( p[begin] == '*' ) {
+      begin ++;
+      continue;
     }
+    int end = begin + 1;
+    while ( p[end] && p[end] != '*' ) end ++;
+    int index = distance(s, search(s + i, s + N, p + begin, p + end, equalC));
+    if ( index == N ) return false;
+    if ( begin == 0 ) {
+      if ( index != 0 ) return false;
+      if ( p[end] == 0 ) return index + end - begin == N;
+    } else {
+      if ( p[end] == 0 ) {
+        int index = distance(s, find_end(s + i, s + N, p + begin, p + end, equalC));
+        return index + end - begin == N;
+      }
+    }
+    i = index + end - begin;
+    begin = end;
   }
-  return false;
+  return true;
 }
 
