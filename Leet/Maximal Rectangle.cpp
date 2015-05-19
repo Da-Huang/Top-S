5fcc472ef5e1ae24727187b3ab89365e3a461018
Da Huang <dhuang.cn@gmail.com>
2015-05-19 12:58:20 +0800

    Maximal Rectangle
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Maximal Rectangle.cpp
+++ Leet/Maximal Rectangle.cpp
@@ -1,48 +1,37 @@
-int __his(int his[], int N) {
-  int stk[N];
-  int stkLen = 0;
+int __maxArea(int h[], int N) {
   int leftMost[N];
-  int rightMost[N];
-  leftMost[0] = 0;
-  stk[stkLen ++] = 0;
-  for (int i = 1; i < N; i ++) {
-    while ( stkLen > 0 && his[i] <= his[stk[stkLen - 1]] ) stkLen --;
-    if ( stkLen == 0 ) leftMost[i] = 0;
-    else leftMost[i] = stk[stkLen - 1] + 1;
-    stk[stkLen ++] = i;
+  stack<int> stk;
+  for (int i = 0; i < N; ++ i) {
+    while (!stk.empty() && h[stk.top()] >= h[i]) stk.pop();
+    leftMost[i] = stk.empty() ? 0 : stk.top() + 1;
+    stk.push(i);
   }
-  stkLen = 0;
-  rightMost[N - 1] = N - 1;
-  stk[stkLen ++] = N - 1;
-  for (int i = N - 2; i >= 0; i --) {
-    while ( stkLen > 0 && his[i] <= his[stk[stkLen - 1]] ) stkLen --;
-    if ( stkLen == 0 ) rightMost[i] = N - 1;
-    else rightMost[i] = stk[stkLen - 1] - 1;
-    stk[stkLen ++] = i;
+  stk = stack<int>();
+  int ans = 0;
+  for (int i = N - 1; i >= 0; -- i) {
+    while (!stk.empty() && h[stk.top()] >= h[i]) stk.pop();
+    int rightMost = stk.empty() ? N - 1 : stk.top() - 1;
+    ans = max(ans, (rightMost - leftMost[i] + 1) * h[i]);
+    stk.push(i);
   }
-  int res = 0;
-  for (int i = 0; i < N; i ++) {
-    res = max(res, his[i] * (rightMost[i] - leftMost[i] + 1));
-  }
-  return res;
+  return ans;
 }
 
-int maximalRectangle(vector<vector<char> > &matrix) {
+int maximalRectangle(vector<vector<char>>& matrix) {
   const int M = matrix.size();
-  if ( M == 0 ) return 0;
+  if (M == 0) return 0;
   const int N = matrix[0].size();
-  if ( N == 0 ) return 0;
-  int his[N];
-  memset(his, 0, sizeof(his));
+  if (N == 0) return 0;
 
-  int res = 0;
-  for (int i = 0; i < M; i ++) {
-    for (int j = 0; j < N; j ++) {
-      if ( matrix[i][j] == '1' ) his[j] ++;
-      else his[j] = 0;
+  int h[N];
+  memset(h, 0, sizeof(h));
+  int ans = 0;
+  for (int i = 0; i < M; ++ i) {
+    for (int j = 0; j < N; ++ j) {
+      if (matrix[i][j] == '1') ++ h[j];
+      else h[j] = 0;
     }
-    res = max(res, __his(his, N));
+    ans = max(ans, __maxArea(h, N));
   }
-  return res;
+  return ans;
 }
-
