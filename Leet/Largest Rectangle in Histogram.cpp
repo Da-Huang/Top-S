7b9465db9d9025cbb6afa3810ec8248ca1109aca
Da Huang <dhuang.cn@gmail.com>
2015-05-19 15:11:14 +0800

    Largest Rectangle in Histogram
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Largest Rectangle in Histogram.cpp
+++ Leet/Largest Rectangle in Histogram.cpp
@@ -1,24 +1,19 @@
-int largestRectangleArea(vector<int> &height) {
+int largestRectangleArea(vector<int>& height) {
   const int N = height.size();
-  if ( N == 0 ) return 0;
-  int leftMost[N], rightMost[N];
-  int stk[N];
-  int stkLen = 0;
-  for (int i = 0; i < N; i ++) {
-    while ( stkLen > 0 && height[i] <= height[stk[stkLen - 1]] ) stkLen --;
-    leftMost[i] = stkLen > 0 ? stk[stkLen - 1] + 1 : 0;
-    stk[stkLen ++] = i;
+  int leftMost[N];
+  stack<int> stk;
+  for (int i = 0; i < N; ++ i) {
+    while (!stk.empty() && height[stk.top()] >= height[i]) stk.pop();
+    leftMost[i] = stk.empty() ? 0 : stk.top() + 1;
+    stk.push(i);
   }
-  stkLen = 0;
-  for (int i = N - 1; i >= 0; i --) {
-    while ( stkLen > 0 && height[i] <= height[stk[stkLen - 1]] ) stkLen --;
-    rightMost[i] = stkLen > 0 ? stk[stkLen - 1] - 1 : N - 1;
-    stk[stkLen ++] = i;
+  stk = stack<int>();
+  int ans = 0;
+  for (int i = N - 1; i >= 0; -- i) {
+    while (!stk.empty() && height[stk.top()] >= height[i]) stk.pop();
+    int rightMost = stk.empty() ? N - 1 : stk.top() - 1;
+    ans = max(ans, (rightMost - leftMost[i] + 1) * height[i]);
+    stk.push(i);
   }
-  int res = 0;
-  for (int i = 0; i < N; i ++) {
-    res = max(res, height[i] * (rightMost[i] + 1 - leftMost[i]));
-  }
-  return res;
+  return ans;
 }
-
