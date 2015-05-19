1b3ebfb82b8f7b4cc0af11eda63b13ba2d9e4c02
Da Huang <dhuang.cn@gmail.com>
2015-05-19 11:18:10 +0800

    Rotate Image
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Rotate Image.cpp
+++ Leet/Rotate Image.cpp
@@ -1,9 +1,10 @@
 // Redo
-void rotate(vector<vector<int> > &matrix) {
+void rotate(vector<vector<int>>& matrix) {
   const int N = matrix.size();
-  if ( N == 0 ) return;
-  for (int i = 0; i <= (N-1)/2; i ++) {
-    for (int j = i; j <= N - 2 - i; j ++) {
+  if (N == 0) return;
+
+  for (int i = 0; i < N/2; ++ i) {
+    for (int j = 0; j < (N+1)/2; ++ j) {
       int tmp = matrix[i][j];
       matrix[i][j] = matrix[N-1-j][i];
       matrix[N-1-j][i] = matrix[N-1-i][N-1-j];
@@ -12,4 +13,3 @@ void rotate(vector<vector<int> > &matrix) {
     }
   }
 }
-
