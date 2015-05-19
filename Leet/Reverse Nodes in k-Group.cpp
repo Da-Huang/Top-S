159e41eaf792d9af1b17c389837f6d950c58126a
Da Huang <dhuang.cn@gmail.com>
2015-05-19 12:11:27 +0800

    Reverse Nodes in k-Group
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Reverse Nodes in k-Group.cpp
+++ Leet/Reverse Nodes in k-Group.cpp
@@ -1,32 +1,19 @@
-// Redo
-ListNode *__reverse(ListNode *HEAD, ListNode *tail) {
-  ListNode *nextHead = tail->next;
-  ListNode *newTail = HEAD->next;
-  while ( newTail->next != nextHead ) {
-    ListNode *node = newTail->next;
-    newTail->next = node->next;
-    node->next = HEAD->next;
-    HEAD->next = node;
-  }
-  return newTail;
-}
-
-ListNode *reverseKGroup(ListNode *head, int k) {
-  if ( k <= 1 ) return head;
+ListNode* reverseKGroup(ListNode* head, int k) {
   ListNode HEAD(0);
   HEAD.next = head;
   ListNode *ptr = &HEAD;
-  while ( true ) {
-    ListNode *q = ptr;
-    int i = 0;
-    while ( i < k ) {
-      if ( q->next == NULL ) break;
-      q = q->next;
-      i ++;
+  while (ptr->next) {
+    int count = 0;
+    for (ListNode *q = ptr; q->next; q = q->next) ++ count;
+    if (count < k) break;
+    ListNode *tail = ptr->next;
+    for (int i = 0; i < k - 1; ++ i) {
+      ListNode *node = tail->next;
+      tail->next = node->next;
+      node->next = ptr->next;
+      ptr->next = node;
     }
-    if ( i < k ) break;
-    ptr = __reverse(ptr, q);
+    ptr = tail;
   }
   return HEAD.next;
 }
-
