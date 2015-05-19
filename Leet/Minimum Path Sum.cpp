12294545154b6597365bb210ca1962d513d24a14
Da Huang <dhuang.cn@gmail.com>
2015-05-19 12:35:30 +0800

    Minimum Path Sum
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Minimum Path Sum.cpp
+++ Leet/Minimum Path Sum.cpp
@@ -1,16 +1,17 @@
-int minPathSum(vector<vector<int> > &grid) {
+int minPathSum(vector<vector<int>>& grid) {
   const int M = grid.size();
-  if ( M == 0 ) return 0;
+  if (M == 0) return 0;
   const int N = grid[0].size();
-  if ( N == 0 ) return 0;
-  int res[M][N];
-  res[0][0] = grid[0][0];
-  for (int i = 1; i < M; i ++) res[i][0] = res[i - 1][0] + grid[i][0];
-  for (int j = 1; j < N; j ++) res[0][j] = res[0][j - 1] + grid[0][j];
-  for (int i = 1; i < M; i ++) {
-    for (int j = 1; j < N; j ++) {
-      res[i][j] = min(res[i - 1][j], res[i][j - 1]) + grid[i][j];
+  if (N == 0) return 0;
+
+  int sum[2][N];
+  sum[0][0] = grid[0][0];
+  for (int j = 1; j < N; ++ j) sum[0][j] = sum[0][j-1] + grid[0][j];
+  for (int i = 1; i < M; ++ i) {
+    sum[i&1][0] = sum[(i-1)&1][0] + grid[i][0];
+    for (int j = 1; j < N; ++ j) {
+      sum[i&1][j] = min(sum[(i-1)&1][j], sum[i&1][j-1]) + grid[i][j];
     }
   }
-  return res[M - 1][N - 1];
+  return sum[(M-1)&1][N-1];
 }
