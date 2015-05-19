3455faafe4078d1111a30b0a67d5aef0d3c734c4
Da Huang <dhuang.cn@gmail.com>
2015-05-19 13:07:12 +0800

    Symmetric Tree
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Symmetric Tree.cpp
+++ Leet/Symmetric Tree.cpp
@@ -1,12 +1,10 @@
-bool __isSymmetric(TreeNode *l, TreeNode *r) {
-  if ( l == NULL && r == NULL ) return true;
-  if ( l == NULL ) return false;
-  if ( r == NULL ) return false;
-  return l->val == r->val && __isSymmetric(l->left, r->right) && __isSymmetric(l->right, r->left);
+bool __isSymmetrix(TreeNode *t1, TreeNode *t2) {
+  if (t1 == NULL && t2 == NULL) return true;
+  if (t1 && t2) return t1->val == t2->val && __isSymmetrix(t1->left, t2->right) && __isSymmetrix(t1->right, t2->left);
+  return false;
 }
 
-bool isSymmetric(TreeNode *root) {
-  if ( root == NULL ) return true;
-  return __isSymmetric(root->left, root->right);
+bool isSymmetric(TreeNode* root) {
+  if (root == NULL) return true;
+  return __isSymmetrix(root->left, root->right);
 }
-
