3bac0c827ba3855554bfa57f49cbda494bbce197
Da Huang <dhuang.cn@gmail.com>
2015-05-19 17:00:16 +0800

    Combinations
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Combinations.cpp
+++ Leet/Combinations.cpp
@@ -17,3 +17,27 @@ vector<vector<int>> combineInner(int n, int k, int start=1) {
 vector<vector<int> > combine(int n, int k) {
   return combineInner(n, k);
 }
+
+
+// #redo
+// #version2
+void __combine(int n, int k, vector<int> &v, vector<vector<int>> &ans) {
+  if (k == 0) {
+    ans.push_back(v);
+    return;
+  }
+
+  int start = v.empty() ? 1 : v.back() + 1;
+  for (int i = start; i + k - 1 <= n; ++ i) {
+    v.push_back(i);
+    __combine(n, k - 1, v, ans);
+    v.pop_back();
+  }
+}
+
+vector<vector<int>> combine(int n, int k) {
+  vector<vector<int>> ans;
+  vector<int> v;
+  __combine(n, k, v, ans);
+  return ans;
+}
