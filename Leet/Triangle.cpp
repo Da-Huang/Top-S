5235f5d35b18a481f74dbb59bd337c4126327062
Da Huang <dhuang.cn@gmail.com>
2015-05-20 09:49:56 +0800

    Triangle
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Triangle.cpp
+++ Leet/Triangle.cpp
@@ -1,15 +1,14 @@
 int minimumTotal(vector<vector<int> > &triangle) {
   const int N = triangle.size();
-  if ( N == 0 ) return 0;
-  int res[N];
-  res[0] = triangle[0][0];
-  for (int i = 1; i < N; i ++) {
-    res[i] = triangle[i][i] + res[i - 1];
-    for (int j = i - 1; j > 0; j --) {
-      res[j] = min(res[j], res[j - 1]) + triangle[i][j];
+  if (N == 0) return 0;
+  int v[N];
+  v[0] = triangle[0][0];
+  for (int i = 1; i < N; ++ i) {
+    v[i] = v[i - 1] + triangle[i][i];
+    for (int j = i - 1; j > 0; -- j) {
+      v[j] = min(v[j], v[j - 1]) + triangle[i][j];
     }
-    res[0] += triangle[i][0];
+    v[0] += triangle[i][0];
   }
-  return *min_element(res, res + N);
+  return *min_element(v, v + N);
 }
-
