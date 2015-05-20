833c80a46069f993dbec174f951797c41fb92fd3
Da Huang <dhuang.cn@gmail.com>
2015-05-20 12:58:40 +0800

    Maximum Depth of Binary Tree
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Maximum Depth of Binary Tree.cpp
+++ Leet/Maximum Depth of Binary Tree.cpp
@@ -1,5 +1,4 @@
-int maxDepth(TreeNode *root) {
-  if ( root == NULL ) return 0;
+int maxDepth(TreeNode* root) {
+  if (root == NULL) return 0;
   return max(maxDepth(root->left), maxDepth(root->right)) + 1;
 }
-
