4d295e744242750bf30ab6c6e6c5b8078ca804a7
Da Huang <dhuang.cn@gmail.com>
2015-05-20 10:17:16 +0800

    Minimum Depth of Binary Tree
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Minimum Depth of Binary Tree.cpp
+++ Leet/Minimum Depth of Binary Tree.cpp
@@ -1,13 +1,7 @@
-int minDepth(TreeNode *root) {
-  if ( root == NULL ) return 0;
-  if ( root->left == NULL && root->right == NULL ) return 1;
-  int res = INT_MAX;
-  if ( root->left ) {
-    res = min(res, minDepth(root->left));
-  }
-  if ( root->right ) {
-    res = min(res, minDepth(root->right));
-  }
-  return res + 1;
+int minDepth(TreeNode* root) {
+  if (root == NULL) return 0;
+  if (root->left == NULL && root->right == NULL) return 1;
+  if (root->left && root->right) return min(minDepth(root->left), minDepth(root->right)) + 1;
+  if (root->left) return minDepth(root->left) + 1;
+  /* root->right */ return minDepth(root->right) + 1;
 }
-
