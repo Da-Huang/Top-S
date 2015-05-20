06cb35f97d7d28a0e5317000436e9b7c9cc5ca77
Da Huang <dhuang.cn@gmail.com>
2015-05-20 09:27:49 +0800

    Binary Tree Zigzag Level Order Traversal
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Binary Tree Zigzag Level Order Traversal.cpp
+++ Leet/Binary Tree Zigzag Level Order Traversal.cpp
@@ -1,29 +1,28 @@
-vector<vector<int> > zigzagLevelOrder(TreeNode *root) {
-  vector<vector<int>> res;
-  if ( root == NULL ) return res;
+// #redo
+vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+  vector<vector<int>> ans;
+  if (root == NULL) return ans;
+
   queue<TreeNode*> q;
   q.push(root);
   q.push(NULL);
-  bool isRight = true;
-  vector<int> subRes;
-  while ( !q.empty() ) {
+  vector<int> v;
+  bool isReverse = false;
+  while (!q.empty()) {
     TreeNode *node = q.front();
     q.pop();
-    if ( node == NULL ) {
-      if ( !isRight ) {
-        reverse(subRes.begin(), subRes.end());
-      }
-      res.push_back(subRes);
-      subRes.clear();
-      isRight = !isRight;
-      if ( q.empty() ) break;
-      q.push(NULL);
-      continue;
-    }
-    subRes.push_back(node->val);
-    if ( node->left ) q.push(node->left);
-    if ( node->right ) q.push(node->right);
-  }
-  return res;
-}
+    if (node == NULL) {
+      if (isReverse) reverse(v.begin(), v.end());
+      isReverse = !isReverse;
+      ans.push_back(v);
+      v.clear();
+      if (!q.empty()) q.push(NULL);
 
+    } else {
+      v.push_back(node->val);
+      if (node->left) q.push(node->left);
+      if (node->right) q.push(node->right);
+    }
+  }
+  return ans;
+}
