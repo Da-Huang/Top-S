f43b913fa753d75ac9ca2968734ff8815545bc2f
Da Huang <dhuang.cn@gmail.com>
2015-05-20 10:29:50 +0800

    Valid Palindrome
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Valid Palindrome.cpp
+++ Leet/Valid Palindrome.cpp
@@ -1,17 +1,14 @@
 bool isPalindrome(string s) {
-  const int N = s.length();
-  int first = 0, last = N - 1;
-
-  while ( first < last && !isalnum(s[first]) ) first ++;
-  while ( first < last && !isalnum(s[last]) ) last --;
-  while ( first < last ) {
-    char c1 = isupper(s[first]) ? tolower(s[first]) : s[first];
-    char c2 = isupper(s[last]) ? tolower(s[last]) : s[last];
-    if ( c1 != c2 ) return false;
-    first ++; last --;
-    while ( first < last && !isalnum(s[first]) ) first ++;
-    while ( first < last && !isalnum(s[last]) ) last --;
+  int i = 0;
+  int j = (int) s.size() - 1;
+  while (i < j) {
+    if (!isalnum(s[i])) ++ i;
+    else if (!isalnum(s[j])) -- j;
+    else if (tolower(s[i]) != tolower(s[j])) return false;
+    else {
+      ++ i;
+      -- j;
+    }
   }
   return true;
 }
-
