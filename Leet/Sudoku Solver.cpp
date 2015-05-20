bd85287b760513dc6447e9255d302e7e9da04069
Da Huang <dhuang.cn@gmail.com>
2015-05-20 10:04:57 +0800

    Sudoku Solver
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Sudoku Solver.cpp
+++ Leet/Sudoku Solver.cpp
@@ -1,48 +1,34 @@
-// Redo
-short __able(vector<vector<char>> &board, int row, int col) {
-  // board[row][col] must be '.'
-  short res = 0xFFFF;
-  for (int i = 0; i < 9; i ++) {
-    if ( board[i][col] == '.' ) continue;
-    int d = board[i][col] - '0';
-    res &= ~(1 << d);
-  }
-  for (int j = 0; j < 9; j ++) {
-    if ( board[row][j] == '.' ) continue;
-    int d = board[row][j] - '0';
-    res &= ~(1 << d);
-  }
-  const int ROW = row / 3;
-  const int COL = col / 3;
-  for (int i = 0; i < 3; i ++) {
-    for (int j = 0; j < 3; j ++) {
-      const int ii = ROW * 3 + i;
-      const int jj = COL * 3 + j;
-      if ( board[ii][jj] == '.' ) continue;
-      int d = board[ii][jj] - '0';
-      res &= ~(1 << d);
-    }
-  }
-  return res;
-}
+// redo
+bool __solveSudoku(vector<vector<char>>& board, int k) {
+  const int N = 9;
+  if (k == N * N) return true;
+  int row = k / N;
+  int col = k % N;
+  if (board[row][col] != '.') return __solveSudoku(board, k + 1);
 
-bool __solveSudoku(vector<vector<char>> &board, int start=0) {
-  if ( start == 81 ) return true;
-  int i = start / 9;
-  int j = start % 9;
-  if ( board[i][j] != '.' ) return __solveSudoku(board, start + 1);
-  short able = __able(board, i, j);
-  for (int k = 1; k <= 9; k ++) {
-    if ( able & (1 << k) ) {
-      board[i][j] = k + '0';
-      if ( __solveSudoku(board, start + 1) ) return true;
+  vector<bool> v(N, true);
+  for (int i = 0; i < N; ++ i) {
+    if (board[i][col] != '.') v[board[i][col] - '1'] = false;
+  }
+  for (int j = 0; j < N; ++ j) {
+    if (board[row][j] != '.') v[board[row][j] - '1'] = false;
+  }
+  for (int i = 0; i < N / 3; ++ i) {
+    for (int j = 0; j < N / 3; ++ j) {
+      char c = board[row-row%3+i][col-col%3+j];
+      if (c != '.') v[c - '1'] = false;
     }
   }
-  board[i][j] = '.';
+  for (int i = 0; i < N; ++ i) {
+    if (v[i]) {
+      board[row][col] = i + '1';
+      if (__solveSudoku(board, k + 1)) return true;
+    }
+  }
+  board[row][col] = '.';
   return false;
 }
 
-void solveSudoku(vector<vector<char> > &board) {
+void solveSudoku(vector<vector<char>>& board) {
   __solveSudoku(board, 0);
 }
-
