cd069efd3089b2a9f725d0ed532cafc3aae874ee
Da Huang <dhuang.cn@gmail.com>
2015-05-22 14:01:18 +0800

    Bitwise AND of Numbers Range
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Bitwise AND of Numbers Range.cpp
+++ Leet/Bitwise AND of Numbers Range.cpp
@@ -1,19 +1,10 @@
 // #redo
 int rangeBitwiseAnd(int m, int n) {
-  int k = 0, t = m;
-  while (t) {
-    t >>= 1;
-    ++ k;
-  }
-  if (k == 0) return 0;
-  k = 1 << (k - 1);
-  if ((n & k) == 0 || ~((k-1) | k) & n) return 0;
-
-  int ans = 0;
+  int k = m ^ n;
+  int mask = -1;
   while (k) {
-    if ((k & m) != (k & n)) break;
-    ans |= k & m;
     k >>= 1;
+    mask <<= 1;
   }
-  return ans;
+  return mask & m;
 }
