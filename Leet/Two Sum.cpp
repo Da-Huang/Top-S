cc31068de4a1af31607e03237ae12cefe3407f8d
Da Huang <dhuang.cn@gmail.com>
2015-05-22 12:48:02 +0800

    Two Sum
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Two Sum.cpp
+++ Leet/Two Sum.cpp
@@ -1,42 +1,32 @@
-// Solution 1
-vector<int> twoSum(vector<int> &numbers, int target) {
-  const int N = numbers.size();
-  int indexes[N];
-  for (int i = 0; i < N; i ++) indexes[i] = i;
-  sort(indexes, indexes + N, [&numbers] (int i, int j) {
-      return numbers[i] < numbers[j];
+// #redo
+vector<int> twoSum(vector<int>& nums, int target) {
+  const int N = nums.size();
+  if (N < 2) return {};
+
+  vector<int> idxs(N);
+  for (int i = 0; i < N; ++ i) idxs[i] = i;
+  sort(idxs.begin(), idxs.end(), [&nums](int i, int j) {
+      return nums[i] < nums[j];
       });
-  vector<int> res;
-  int first = 0, last = N - 1;
-  while ( first < last ) {
-    if ( numbers[indexes[first]] + numbers[indexes[last]] > target ) last --;
-    else if ( numbers[indexes[first]] + numbers[indexes[last]] < target ) first ++;
-    else { // ==
-      res.push_back(min(indexes[first], indexes[last]) + 1);
-      res.push_back(max(indexes[first], indexes[last]) + 1);
-      return res;
-    }
+  int i = 0, j = N - 1;
+  while (i < j) {
+    if (nums[idxs[i]] + nums[idxs[j]] > target) -- j;
+    else if (nums[idxs[i]] + nums[idxs[j]] < target) ++ i;
+    else return {min(idxs[i],idxs[j]) + 1, max(idxs[i],idxs[j]) + 1};
   }
-  return res;
+  return {};
 }
 
-// Solution 2
-vector<int> twoSum(vector<int> &numbers, int target) {
-  const int N = numbers.size();
-  unordered_multimap<int, int> mapping;
-  for (int i = 0; i < N; i ++) mapping.insert(make_pair(numbers[i], i));
-  vector<int> res;
-  for (int i = 0; i < N; i ++) {
-    auto range = mapping.equal_range(target - numbers[i]);
-    auto begin = range.first;
-    auto end = range.second;
-    for (auto p = begin; p != end; p ++) {
-      if ( p->second != i ) {
-        res.push_back(min(i, p->second) + 1);
-        res.push_back(max(i, p->second) + 1);
-      }
-    }
-  }
-  return res;
-}
+// #solution2
+vector<int> twoSum(vector<int>& nums, int target) {
+  const int N = nums.size();
+  if (N < 2) return {};
 
+  unordered_map<int, int> mapping;
+  for (int i = 0; i < N; ++ i) {
+    auto it = mapping.find(target - nums[i]);
+    if (it != mapping.end()) return {it->second + 1, i + 1};
+    mapping[nums[i]] = i;
+  }
+  return {};
+}
