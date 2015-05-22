8abdc18f89c67271219d6e7c4e45e0b088ff3673
Da Huang <dhuang.cn@gmail.com>
2015-05-22 13:00:04 +0800

    Construct Binary Tree from Inorder and Postorder Traversal
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Construct Binary Tree from Inorder and Postorder Traversal.cpp
+++ Leet/Construct Binary Tree from Inorder and Postorder Traversal.cpp
@@ -1,18 +1,18 @@
-// Dangerous
-TreeNode *__build(vector<int> &inorder, vector<int> &postorder, int inFirst, int postFirst, int n) {
-  if ( n == 0 ) return NULL;
-  TreeNode *root = new TreeNode(postorder[postFirst + n - 1]);
-  int i;
-  for (i = inFirst; i < inFirst + n; i ++) {
-    if ( inorder[i] == root->val ) break;
-  }
-  root->left = __build(inorder, postorder, inFirst, postFirst, i - inFirst);
-  root->right = __build(inorder, postorder, i + 1, postFirst + i - inFirst, n - i + inFirst - 1);
+// #redo
+TreeNode* __buildTree(vector<int>& inorder, vector<int>& postorder,
+    int inBegin, int postBegin, int n, unordered_map<int, int> &mapping) {
+  if (n == 0) return NULL;
+  TreeNode *root = new TreeNode(postorder[postBegin + n - 1]);
+  int k = mapping[root->val];
+  root->left = __buildTree(inorder, postorder, inBegin, postBegin, k - inBegin, mapping);
+  root->right = __buildTree(inorder, postorder, k + 1, postBegin + k - inBegin, n - 1 - k + inBegin, mapping);
   return root;
 }
 
-TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
+TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+  if (inorder.size() != postorder.size()) return NULL;
+  unordered_map<int, int> mapping;
   const int N = inorder.size();
-  return __build(inorder, postorder, 0, 0, N);
+  for (int i = 0; i < N; ++ i) mapping[inorder[i]] = i;
+  return __buildTree(inorder, postorder, 0, 0, N, mapping);
 }
-
