760fd0aee49e4768cdbc4cfd761a8ae633c1a25f
Da Huang <dhuang.cn@gmail.com>
2015-05-22 10:06:37 +0800

    Insert Interval
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Insert Interval.cpp
+++ Leet/Insert Interval.cpp
@@ -1,21 +1,14 @@
-// Redo
-vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
-  vector<Interval> res;
-  const int N = intervals.size();
-  int i = 0;
-  while ( i < N && intervals[i].end < newInterval.start ) res.push_back(intervals[i ++]);
-  if ( i >= N || intervals[i].start > newInterval.end ) {
-    res.push_back(newInterval);
-  } else {
-    int end = i;
-    while ( end < N && intervals[end].start <= newInterval.end ) end ++;
-    res.push_back(Interval(
-          min(intervals[i].start, newInterval.start),
-          max(intervals[end - 1].end, newInterval.end)
-          ));
-    i = end;
+// #redo
+vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
+  vector<Interval> ans;
+  size_t i = 0;
+  while (i < intervals.size() && intervals[i].end < newInterval.start) ans.push_back(intervals[i ++]);
+  while (i < intervals.size() && intervals[i].start <= newInterval.end) {
+    newInterval.start = min(newInterval.start, intervals[i].start);
+    newInterval.end = max(newInterval.end, intervals[i].end);
+    ++ i;
   }
-  while ( i < N ) res.push_back(intervals[i ++]);
-  return res;
+  ans.push_back(newInterval);
+  while (i < intervals.size()) ans.push_back(intervals[i ++]);
+  return ans;
 }
-
