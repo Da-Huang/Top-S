58e72d17f39e836e82ceac053e3bf298993653f5
Da Huang <dhuang.cn@gmail.com>
2015-05-23 20:12:26 +0800

    Permutations II
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Permutations II.cpp
+++ Leet/Permutations II.cpp
@@ -35,3 +35,42 @@ vector<vector<int> > permuteUnique(vector<int> &num) {
   return __permute(num, visited);
 }
 
+
+// #redo
+// #version2
+void __permute(vector<int> &nums, size_t n, size_t last, vector<bool> &visited, vector<int> &v, vector<vector<int>> &ans) {
+  if (n == nums.size()) {
+    ans.push_back(v);
+    return;
+  }
+  size_t begin = 0;
+  while (begin < nums.size()) {
+    if (visited[begin]) {
+      ++ begin;
+      continue;
+    }
+    size_t end = begin + 1;
+    while (end < nums.size() && nums[begin] == nums[end]) ++ end;
+    if (last != end) {
+      for (size_t i = begin; i < end; ++ i) {
+        visited[i] = true;
+        v.push_back(nums[i]);
+        __permute(nums, n + i - begin + 1, end, visited, v, ans);
+      }
+      for (size_t i = begin; i < end; ++ i) {
+        visited[i] = false;
+        v.pop_back();
+      }
+    }
+    begin = end;
+  }
+}
+
+vector<vector<int>> permuteUnique(vector<int>& nums) {
+  sort(nums.begin(), nums.end());
+  vector<bool> visited(nums.size(), false);
+  vector<int> v;
+  vector<vector<int>> ans;
+  __permute(nums, 0, -1, visited, v, ans);
+  return ans;
+}
--- Leet/Permutations.cpp
+++ Leet/Permutations.cpp
@@ -25,47 +25,7 @@ vector<vector<int> > permute(vector<int> &num) {
 }
 
 
-// #redo
-// #version2: for duplicate cases
-void __permute(vector<int> &nums, size_t n, int last, vector<bool> &visited, vector<int> &v, vector<vector<int>> &ans) {
-  if (n == nums.size()) {
-    ans.push_back(v);
-    return;
-  }
-  size_t begin = 0;
-  while (begin < nums.size()) {
-    if (visited[begin]) {
-      ++ begin;
-      continue;
-    }
-    size_t end = begin + 1;
-    while (end < nums.size() && nums[begin] == nums[end]) ++ end;
-    if (last != end) {
-      for (size_t i = begin; i < end; ++ i) {
-        visited[i] = true;
-        v.push_back(nums[i]);
-        __permute(nums, n + i - begin + 1, end, visited, v, ans);
-      }
-      for (size_t i = begin; i < end; ++ i) {
-        visited[i] = false;
-        v.pop_back();
-      }
-    }
-    begin = end;
-  }
-}
-
-vector<vector<int>> permute(vector<int>& nums) {
-  sort(nums.begin(), nums.end());
-  vector<bool> visited(nums.size(), false);
-  vector<int> v;
-  vector<vector<int>> ans;
-  __permute(nums, 0, -1, visited, v, ans);
-  return ans;
-}
-
-
-// #version3
+// #version2
 void __permute(vector<int> &nums, size_t n, vector<bool> &visited, vector<int> &v, vector<vector<int>> &ans) {
   if (n == nums.size()) {
     ans.push_back(v);
