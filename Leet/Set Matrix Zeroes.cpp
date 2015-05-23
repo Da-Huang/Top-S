483b7c24887f1acbea020f515806a17c0f244540
Da Huang <dhuang.cn@gmail.com>
2015-05-23 19:31:05 +0800

    Set Matrix Zeroes
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Set Matrix Zeroes.cpp
+++ Leet/Set Matrix Zeroes.cpp
@@ -1,40 +1,36 @@
-void setZeroes(vector<vector<int> > &matrix) {
+void setZeroes(vector<vector<int>>& matrix) {
   const int M = matrix.size();
-  if ( M == 0 ) return;
+  if (M == 0) return;
   const int N = matrix[0].size();
-  if ( N == 0 ) return;
-  bool zeroFirstLine = false;
-  for (int j = 0; j < N; j ++) {
-    if ( matrix[0][j] == 0 ) {
-      zeroFirstLine = true;
+  if (N == 0) return;
+
+  bool isZero = false;
+  for (int j = 0; j < N; ++ j) {
+    if (matrix[0][j] == 0) {
+      isZero = true;
       break;
     }
   }
-  for (int i = 1; i < M; i ++) {
-    bool zeroThisLine = false;
-    for (int j = 0; j < N; j ++) {
-      if ( matrix[i][j] == 0 ) {
+
+  for (int i = 1; i < M; ++ i) {
+    bool hasZero = false;
+    for (int j = 0; j < N; ++ j) {
+      if (matrix[i][j] == 0) {
         matrix[0][j] = 0;
-        zeroThisLine = true;
+        hasZero = true;
       }
     }
-    if ( zeroThisLine ) {
-      for (int j = 0; j < N; j ++) {
-        matrix[i][j] = 0;
-      }
+    if (hasZero) {
+      for (int j = 0; j < N; ++ j) matrix[i][j] = 0;
     }
   }
-  for (int j = 0; j < N; j ++) {
-    if ( matrix[0][j] == 0 ) {
-      for (int i = 1; i < M; i ++) {
-        matrix[i][j] = 0;
-      }
+
+  for (int j = 0; j < N; ++ j) {
+    if (matrix[0][j] == 0) {
+      for (int i = 1; i < M; ++ i) matrix[i][j] = 0;
     }
   }
-  if ( zeroFirstLine ) {
-    for (int j = 0; j < N; j ++) {
-      matrix[0][j] = 0;
-    }
+  if (isZero) {
+    for (int j = 0; j < N; ++ j) matrix[0][j] = 0;
   }
 }
-
