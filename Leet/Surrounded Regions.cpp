20abac4a09f772ccaf67f7bd6200f5febbba780a
Da Huang <dhuang.cn@gmail.com>
2015-05-23 12:49:17 +0800

    Surrounded Regions
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Surrounded Regions.cpp
+++ Leet/Surrounded Regions.cpp
@@ -1,40 +1,37 @@
-// Redo
-void dfs(vector<vector<char>> &board, int i, int j) {
-  const int M = board.size();
-  const int N = board[0].size();
+// #redo
+void __solve(vector<vector<char>> &board, int M, int N, int i, int j) {
   stack<pair<int, int>> stk;
   stk.push(make_pair(i, j));
-  while ( !stk.empty() ) {
+  while (!stk.empty()) {
     auto p = stk.top();
     stk.pop();
-    i = p.first; j = p.second;
-    if ( board[i][j] != 'O' ) continue;
-    board[i][j] = '#';
-    if ( i > 0 ) stk.push(make_pair(i - 1, j));
-    if ( j > 0 ) stk.push(make_pair(i, j - 1));
-    if ( i < M - 1 ) stk.push(make_pair(i + 1, j));
-    if ( j < N - 1 ) stk.push(make_pair(i, j + 1));
+    if (board[p.first][p.second] != 'O') continue;
+    board[p.first][p.second] = 'T';
+    if (p.first > 0) stk.push(make_pair(p.first-1, p.second));
+    if (p.second > 0) stk.push(make_pair(p.first, p.second-1));
+    if (p.first+1 < M) stk.push(make_pair(p.first+1, p.second));
+    if (p.second+1 < N) stk.push(make_pair(p.first, p.second+1));
   }
 }
 
-void solve(vector<vector<char>> &board) {
+void solve(vector<vector<char>>& board) {
   const int M = board.size();
-  if ( M == 0 ) return;
+  if (M == 0) return;
   const int N = board[0].size();
-  if ( N == 0 ) return;
-  for (int i = 0; i < M; i ++) {
-    dfs(board, i, 0);
-    dfs(board, i, N - 1);
+  if (N == 0) return;
+
+  for (int i = 0; i < M; ++ i) {
+    __solve(board, M, N, i, 0);
+    __solve(board, M, N, i, N-1);
   }
-  for (int j = 1; j < N - 1; j ++) {
-    dfs(board, 0, j);
-    dfs(board, M - 1, j);
+  for (int j = 0; j < N; ++ j) {
+    __solve(board, M, N, 0, j);
+    __solve(board, M, N, M-1, j);
   }
   for (auto &v : board) {
-    for (auto &c : v) {
-      if ( c == '#' ) c = 'O';
-      else if ( c == 'O' ) c = 'X';
+    for (char &c : v) {
+      if (c == 'T') c = 'O';
+      else if (c == 'O') c = 'X';
     }
   }
 }
-
