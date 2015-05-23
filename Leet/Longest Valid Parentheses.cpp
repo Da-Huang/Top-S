15d9da05432172ad2570a9a1a5b861246d16f2b0
Da Huang <dhuang.cn@gmail.com>
2015-05-23 20:04:27 +0800

    Permutations
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Longest Valid Parentheses.cpp
+++ Leet/Longest Valid Parentheses.cpp
@@ -1,20 +1,18 @@
-// Redo
+// #redo
 int longestValidParentheses(string s) {
-  const int N = s.length();
-  int stk[N];
-  int stkLen = 0;
-  int res = 0;
+  const int N = s.size();
+
+  stack<int> stk;
+  int ans = 0;
   int begin = 0;
-  for (int i = 0; i < N; i ++) {
-    if ( s[i] == '(' ) stk[stkLen ++] = i;
-    else if ( stkLen > 0 ) {
-      stkLen --;
-      const int len = stkLen == 0 ? i + 1 - begin : i - stk[stkLen - 1];
-      res = max(res, len);
-    } else {
-      begin = i + 1;
+  for (int i = 0; i < N; ++ i) {
+    if (s[i] == '(') stk.push(i);
+    else if (stk.empty()) begin = i + 1;
+    else {
+      stk.pop();
+      int base = stk.empty() ? begin : stk.top() + 1;
+      ans = max(ans, i - base + 1);
     }
   }
-  return res;
+  return ans;
 }
-
--- Leet/Permutations.cpp
+++ Leet/Permutations.cpp
@@ -23,3 +23,70 @@ vector<vector<int> > permute(vector<int> &num) {
   vector<bool> selected(num.size(), false);
   return permute(num, selected);
 }
+
+
+// #redo
+// #version2: for duplicate cases
+void __permute(vector<int> &nums, size_t n, int last, vector<bool> &visited, vector<int> &v, vector<vector<int>> &ans) {
+  if (n == nums.size()) {
+    ans.push_back(v);
+    return;
+  }
+  size_t begin = 0;
+  while (begin < nums.size()) {
+    if (visited[begin]) {
+      ++ begin;
+      continue;
+    }
+    size_t end = begin + 1;
+    while (end < nums.size() && nums[begin] == nums[end]) ++ end;
+    if (last != end) {
+      for (size_t i = begin; i < end; ++ i) {
+        visited[i] = true;
+        v.push_back(nums[i]);
+        __permute(nums, n + i - begin + 1, end, visited, v, ans);
+      }
+      for (size_t i = begin; i < end; ++ i) {
+        visited[i] = false;
+        v.pop_back();
+      }
+    }
+    begin = end;
+  }
+}
+
+vector<vector<int>> permute(vector<int>& nums) {
+  sort(nums.begin(), nums.end());
+  vector<bool> visited(nums.size(), false);
+  vector<int> v;
+  vector<vector<int>> ans;
+  __permute(nums, 0, -1, visited, v, ans);
+  return ans;
+}
+
+
+// #version3
+void __permute(vector<int> &nums, size_t n, vector<bool> &visited, vector<int> &v, vector<vector<int>> &ans) {
+  if (n == nums.size()) {
+    ans.push_back(v);
+    return;
+  }
+
+  for (size_t i = 0; i < nums.size(); ++ i) {
+    if (!visited[i]) {
+      visited[i] = true;
+      v.push_back(nums[i]);
+      __permute(nums, n+1, visited, v, ans);
+      visited[i] = false;
+      v.pop_back();
+    }
+  }
+}
+
+vector<vector<int>> permute(vector<int>& nums) {
+  vector<bool> visited(nums.size(), false);
+  vector<int> v;
+  vector<vector<int>> ans;
+  __permute(nums, 0, visited, v, ans);
+  return ans;
+}
