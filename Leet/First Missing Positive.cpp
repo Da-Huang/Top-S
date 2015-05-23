96362a13b215b4c69c095171b2e5cf36f1fa113d
Da Huang <dhuang.cn@gmail.com>
2015-05-23 13:28:17 +0800

    First Missing Positive
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/First Missing Positive.cpp
+++ Leet/First Missing Positive.cpp
@@ -1,12 +1,14 @@
-int firstMissingPositive(int A[], int n) {
-  for (int i = 0; i < n; i ++) {
-    while ( A[i] <= n && A[i] > 0 && A[i] != A[A[i] - 1] ) {
-      swap(A[i], A[A[i] - 1]);
-    }
-  }
-  for (int i = 0; i < n; i ++) {
-    if ( A[i] != i + 1 ) return i + 1;
-  }
-  return n + 1;
-}
+int firstMissingPositive(vector<int>& nums) {
+  const int N = nums.size();
+  size_t i = 0;
+  while (i < N) {
+    if (nums[i] > 0 && nums[i] <= N && nums[nums[i] - 1] != nums[i]) {
+      swap(nums[nums[i] - 1], nums[i]);
 
+    } else ++ i;
+  }
+  for (int i = 0; i < N; ++ i) {
+    if (nums[i] != i + 1) return i + 1;
+  }
+  return N + 1;
+}
