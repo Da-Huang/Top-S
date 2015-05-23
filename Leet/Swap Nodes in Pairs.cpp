c26c1b16b1ca9ee8be7571543c6cb9567430e16c
Da Huang <dhuang.cn@gmail.com>
2015-05-23 16:07:26 +0800

    Swap Nodes in Pairs
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Swap Nodes in Pairs.cpp
+++ Leet/Swap Nodes in Pairs.cpp
@@ -1,15 +1,15 @@
-ListNode *swapPairs(ListNode *head) {
+ListNode* swapPairs(ListNode* head) {
   ListNode HEAD(0);
   HEAD.next = head;
   ListNode *ptr = &HEAD;
-  while ( ptr->next && ptr->next->next ) {
-    ListNode *node1 = ptr->next;
-    ListNode *node2 = node1->next;
-    ptr->next = node2;
-    node1->next = node2->next;
-    node2->next = node1;
-    ptr = node1;
+  while (ptr->next) {
+    ListNode *q = ptr->next;
+    ListNode *node = q->next;
+    if (node == NULL) break;
+    q->next = node->next;
+    node->next = q;
+    ptr->next = node;
+    ptr = q;
   }
   return HEAD.next;
 }
-
