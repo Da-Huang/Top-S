df67e49009601a424c16c819b8c474d4ea7466f5
Da Huang <dhuang.cn@gmail.com>
2015-05-23 16:03:42 +0800

    Search Insert Position
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Search Insert Position.cpp
+++ Leet/Search Insert Position.cpp
@@ -1,11 +1,10 @@
 int searchInsert(vector<int>& nums, int target) {
-  int N = nums.size();
-  if (N == 0) return 0;
-  int first = 0, last = N - 1;
+  const int N = nums.size();
+  int first = 0, last = N;
   while (first < last) {
     int mid = first + (last - first) / 2;
     if (nums[mid] < target) first = mid + 1;
-    else last = mid - 1;
+    else last = mid;
   }
-  return nums[first] >= target ? first : first + 1;
+  return first;
 }
