11a67da78c4d5c708d4a773d3742f3cb1b06df20
Da Huang <dhuang.cn@gmail.com>
2015-05-23 11:25:01 +0800

    Jump Game
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Jump Game.cpp
+++ Leet/Jump Game.cpp
@@ -1,12 +1,8 @@
-bool canJump(int A[], int n) {
-  if ( n == 0 ) return true;
-  int res[n];
-  res[0] = true;
-  int end = 1;
-  for (int i = 0; i < n; i ++) {
-    if ( i >= end ) return false;
-    while ( end < n && i + A[i] >= end ) res[end ++] = true;
+bool canJump(vector<int>& nums) {
+  size_t k = 0;
+  for (size_t i = 0; i < nums.size(); ++ i) {
+    if (k < i) return false;
+    k = max(k, i + nums[i]);
   }
   return true;
 }
-
