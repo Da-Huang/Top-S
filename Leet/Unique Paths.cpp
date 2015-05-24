80a91c1159be6e2ccbd1f57894c16422f0d715e6
Da Huang <dhuang.cn@gmail.com>
2015-05-24 14:08:20 +0800

    Unique Paths
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Unique Paths.cpp
+++ Leet/Unique Paths.cpp
@@ -1,14 +1,23 @@
 int uniquePaths(int m, int n) {
-  n = m + n - 2;
-  m = m - 1;
-  // get C(n, m)
-  long long res = 1;
-  for (int i = 1; i <= m; i ++) {
-    int a = n - m + i;
-    int b = i;
-    res *= a;
-    res /= b;
+  long long ans = 1;
+  for (int i = 1; i <= m - 1; ++ i) {
+    ans *= n + i - 1;
+    ans /= i;
   }
-  return res;
+  return ans;
 }
 
+// #version2
+int uniquePaths(int m, int n) {
+  if (m == 0 || n == 0) return 0;
+  int ans[2][n];
+  for (int j = 0; j < n; ++ j) ans[0][j] = 1;
+
+  for (int i = 1; i < m; ++ i) {
+    ans[i&1][0] = 1;
+    for (int j = 1; j < n; ++ j) {
+      ans[i&1][j] = ans[i&1][j-1] + ans[(i-1)&1][j];
+    }
+  }
+  return ans[(m-1)&1][n-1];
+}
