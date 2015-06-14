05891641bef0f38ece4607291980283b09608a5f
Da Huang <dhuang.cn@gmail.com>
2015-06-14 19:42:44 +0800

    Merge Intervals
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Merge Intervals.cpp
+++ Leet/Merge Intervals.cpp
@@ -1,22 +1,20 @@
 // Redo
-vector<Interval> merge(vector<Interval> &intervals) {
+vector<Interval> merge(vector<Interval>& intervals) {
   sort(intervals.begin(), intervals.end(), [] (const Interval &i1, const Interval &i2) {
       return i1.start < i2.start;
       });
-  vector<Interval> res;
-  int begin = 0;
-  const int N = intervals.size();
-  while ( begin < N ) {
-    int end = begin + 1;
-    int s = intervals[begin].start;
-    int e = intervals[begin].end;
-    while ( end < N && intervals[end].start <= e ) {
-      e = max(e, intervals[end].end);
-      end ++;
-    }
-    res.push_back(Interval(s, e));
-    begin = end;
-  }
-  return res;
-}
+  vector<Interval> ans;
+  if (intervals.empty()) return ans;
 
+  Interval tmp = intervals.front();
+  for (size_t i = 1; i < intervals.size(); ++ i) {
+    if (intervals[i].start <= tmp.end) {
+      tmp.end = max(intervals[i].end, tmp.end);
+    } else {
+      ans.push_back(tmp);
+      tmp = intervals[i];
+    }
+  }
+  ans.push_back(tmp);
+  return ans;
+}
