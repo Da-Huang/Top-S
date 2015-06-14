917f155bfddee92691aa23320166c5cf8a86751c
Da Huang <dhuang.cn@gmail.com>
2015-06-14 19:44:57 +0800

    Roman to Integer
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Roman to Integer.cpp
+++ Leet/Roman to Integer.cpp
@@ -1,5 +1,4 @@
 int romanToInt(string s) {
-  const int N = s.length();
   int ROMAN[128];
   ROMAN['I'] = 1;
   ROMAN['V'] = 5;
@@ -8,18 +7,17 @@ int romanToInt(string s) {
   ROMAN['C'] = 100;
   ROMAN['D'] = 500;
   ROMAN['M'] = 1000;
-  int res = 0;
-  int i = 0;
-  while ( i < N ) {
-    if ( i + 1 < N ) {
-      if ( ROMAN[s[i]] * 5 == ROMAN[s[i + 1]] || ROMAN[s[i]] * 10 == ROMAN[s[i + 1]] ) {
-        res += ROMAN[s[i + 1]] - ROMAN[s[i]];
-        i += 2;
-        continue;
-      }
+
+  int ans = 0;
+  size_t i = 0;
+  while (i + 1 < s.size()) {
+    if (ROMAN[s[i]] * 5 == ROMAN[s[i+1]] || ROMAN[s[i]] * 10 == ROMAN[s[i+1]]) {
+      ans += ROMAN[s[i+1]] - ROMAN[s[i]];
+      i += 2;
+    } else {
+      ans += ROMAN[s[i ++]];
     }
-    res += ROMAN[s[i]];
-    i ++;
   }
-  return res;
+  if (i < s.size()) ans += ROMAN[s[i]];
+  return ans;
 }
