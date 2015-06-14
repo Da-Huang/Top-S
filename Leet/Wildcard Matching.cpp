75939c1bdc45f7b1b62dcf7ff75d8ef8f8d86020
Da Huang <dhuang.cn@gmail.com>
2015-06-14 23:35:20 +0800

    Wildcard Matching
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Wildcard Matching.cpp
+++ Leet/Wildcard Matching.cpp
@@ -1,4 +1,4 @@
-// Redo
+// #redo
 bool isMatch(const char *s, const char *p) {
   const int N = strlen(s);
   if ( *p == 0 ) return N == 0;
@@ -29,6 +29,42 @@ bool isMatch(const char *s, const char *p) {
     i = index + end - begin;
     begin = end;
   }
+  return true;
+}
+
+
+// #version2
+bool isMatch(string s, string p) {
+  auto equalMatch = [](char a, char b) {
+    return a == '?' || b == '?' || a == b;
+  };
+  const int NS = s.size();
+  const int NP = p.size();
+
+  int si = 0, pi = -1, pj;
+  for (int i = 0; i <= NP; ++ i) {
+    if (i == NP || p[i] == '*') {
+      if (pi < 0) continue;
+      pj = i;
+      if (pi == 0) {
+        if (s.size() >= pj && equal(p.begin(), p.begin()+pj, s.begin(), equalMatch)) {
+          si = pj - pi;
+          if (pj == NP) return si == NS;
+        } else return false;
+      } else if (pj == NP) {
+        return find_end(s.begin()+si, s.end(), p.begin()+pi, p.end(), equalMatch)+pj-pi == s.end();
+      } else {
+        auto it = search(s.begin()+si, s.end(), p.begin()+pi, p.begin()+pj, equalMatch);
+        if (it == s.end()) return false;
+        si = distance(s.begin(), it) + pj - pi;
+      }
+      pi = -1;
+
+    } else if (pi < 0) {
+      pi = i;
+    }
+  }
+  if (p.empty() && !s.empty()) return false;
   return true;
 }
 
