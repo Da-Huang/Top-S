ac92d04484ff674db40ce07a4f574839a2d79556
Da Huang <dhuang.cn@gmail.com>
2015-06-14 21:48:31 +0800

    Word Break II
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Word Break II.cpp
+++ Leet/Word Break II.cpp
@@ -1,5 +1,4 @@
-vector<string> wordBreakInner(const string &s, const unordered_set<string> &dict, 
-    vector<string> cache[], vector<bool> &cacheValid, int start=0) {
+vector<string> wordBreakInner(const string &s, const unordered_set<string> &dict, vector<string> cache[], vector<bool> &cacheValid, int start=0) {
   const int N = s.length();
   vector<string> res;
   if ( start == N ) {
@@ -26,4 +25,36 @@ vector<string> wordBreak(string s, unordered_set<string> &dict) {
   vector<string> cache[s.length()];
   vector<bool> cacheValid(s.length(), false);
   return wordBreakInner(s, dict, cache, cacheValid);
+}
+
+
+// #redo
+// #version2
+bool __wordBreak(string &s, unordered_set<string>& wordDict, size_t begin, string &v, vector<bool> &able, vector<string> &ans) {
+  if (begin == s.size()) {
+    ans.push_back(v);
+    return true;
+  }
+  if (!able[begin]) return false;
+
+  bool ansAble = false;
+  for (size_t end = begin + 1; end <= s.size(); ++ end) {
+    string word = s.substr(begin, end - begin);
+    if (wordDict.find(word) != wordDict.end()) {
+      size_t size = v.size();
+      if (!v.empty()) v += " ";
+      v += word;
+      ansAble |= __wordBreak(s, wordDict, end, v, able, ans);
+      v.resize(size);
+    }
+  }
+  return able[begin] = ansAble;
+}
+
+vector<string> wordBreak(string s, unordered_set<string>& wordDict) {
+  vector<string> ans;
+  string v;
+  vector<bool> able(s.size(), true);
+  __wordBreak(s, wordDict, 0, v, able, ans);
+  return ans;
 }
