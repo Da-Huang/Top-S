dee73a59e05f40423bda9e7cd459207872597547
Da Huang <dhuang.cn@gmail.com>
2015-06-15 19:03:52 +0800

    Climbing Stairs
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Climbing Stairs.cpp
+++ Leet/Climbing Stairs.cpp
@@ -1,11 +1,7 @@
 int climbStairs(int n) {
-  if ( n <= 1 ) return 1;
-  int res[3];
-  res[0] = 1;
-  res[1] = 1;
-  for (int i = 2; i <= n; i ++) {
-    res[i%3] = res[(i - 2)%3] + res[(i - 1)%3];
+  int ans[2] = {1, 1};
+  for (int i = 2; i <= n; ++ i) {
+    ans[i&1] += ans[(i+1)&1];
   }
-  return res[n%3];
+  return ans[n&1];
 }
-
