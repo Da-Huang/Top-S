64fbe3f2f56de2250d9b2d42dc8282225da6849b
Da Huang <dhuang.cn@gmail.com>
2015-06-15 18:58:03 +0800

    Binary Tree Inorder Traversal
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Binary Tree Inorder Traversal.cpp
+++ Leet/Binary Tree Inorder Traversal.cpp
@@ -1,3 +1,28 @@
+// #redo
+vector<int> postorderTraversal(TreeNode* root) {
+  vector<int> ans;
+  TreeNode *rl = root;
+  TreeNode *pre = NULL;
+  stack<TreeNode*> stk;
+  while (rl || !stk.empty()) {
+    while (rl) {
+      stk.push(rl);
+      rl = rl->left;
+    }
+
+    TreeNode *node = stk.top();
+    if (node->right == pre || node->right == NULL) {
+      pre = node;
+      stk.pop();
+      ans.push_back(node->val);
+
+    } else rl = node->right;
+  }
+  return ans;
+}
+
+
+// #solution2
 vector<int> inorderTraversal(TreeNode *root) {
   vector<int> res;
   TreeNode *ptr = root;
@@ -22,4 +47,3 @@ vector<int> inorderTraversal(TreeNode *root) {
   }
   return res;
 }
-
