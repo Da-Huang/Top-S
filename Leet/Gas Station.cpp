15ffc645811493973e00573535b50ee14cb7261b
Da Huang <dhuang.cn@gmail.com>
2015-06-15 13:38:24 +0800

    Gas Station
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Gas Station.cpp
+++ Leet/Gas Station.cpp
@@ -1,18 +1,16 @@
-int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
   const int N = gas.size();
-  if ( N == 0 ) return 0;
-  int begin = 0;
-  int cur = 0;
-  for (int i = 0; i < 2 * N - 1; i ++) {
-    cur += gas[i%N] - cost[i%N];
-    if ( cur >= 0 ) {
-      if ( i - begin + 1 == N ) return begin;
+  int ans = 0;
+  int sum = 0;
+  for (int i = 0; i < 2 * N; ++ i) {
+    sum += gas[i%N] - cost[i%N];
+    if (sum < 0) {
+      ans = i + 1;
+      sum = 0;
 
-    } else {
-      begin = i + 1;
-      cur = 0;
+    } else if (i - ans + 1 == N) {
+      return ans;
     }
   }
   return -1;
 }
-
