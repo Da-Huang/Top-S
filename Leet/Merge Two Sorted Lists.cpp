d6025516c816c45ad36ebbfa38da6a772ae39932
Da Huang <dhuang.cn@gmail.com>
2015-06-15 16:05:00 +0800

    Merge Two Sorted Lists
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Merge Two Sorted Lists.cpp
+++ Leet/Merge Two Sorted Lists.cpp
@@ -1,22 +1,19 @@
-ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
   ListNode HEAD(0);
   ListNode *tail = &HEAD;
-  while ( l1 && l2 ) {
-    if ( l1->val <= l2->val ) {
-      ListNode *node = l1;
+  while (l1 && l2) {
+    if (l1->val < l2->val) {
+      tail->next = l1;
+      tail = l1;
       l1 = l1->next;
-      tail->next = node;
-      tail = node;
 
     } else {
-      ListNode *node = l2;
+      tail->next = l2;
+      tail = l2;
       l2 = l2->next;
-      tail->next = node;
-      tail = node;
     }
   }
-  if ( l1 ) tail->next = l1;
-  else if ( l2 ) tail->next = l2;
+  if (l1) tail->next = l1;
+  else if (l2) tail->next = l2;
   return HEAD.next;
 }
-
