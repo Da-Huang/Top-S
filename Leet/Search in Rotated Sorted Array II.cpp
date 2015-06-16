5433c177c3ea2c5b10c8ba528f1f4fae73308a88
Da Huang <dhuang.cn@gmail.com>
2015-06-16 16:37:10 +0800

    Search in Rotated Sorted Array II
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Search in Rotated Sorted Array II.cpp
+++ Leet/Search in Rotated Sorted Array II.cpp
@@ -1,37 +1,27 @@
-int __find(int A[], int n) {
-  int first = 0, last = n - 1;
-  while ( first < last ) {
-    const int mid = (first + last) / 2;
-    if ( A[mid] > A[0] ) first = mid + 1;
-    else if ( A[mid] < A[0] ) last = mid - 1;
-    else {
-      int i = mid - 1;
-      while ( i >= first && A[i] == A[0] ) i --;
-      if ( i < first ) first = mid + 1;
-      else if ( A[i] > A[0] ) return i + 1;
-      else last = i - 1;
-    }
-  }
-  return first;
-}
-
-bool __search(int A[], int first, int last, int target) {
-  while ( first <= last ) {
-    const int mid = (first + last) / 2;
-    if ( A[mid] > target ) last = mid - 1;
-    else if ( A[mid] < target ) first = mid + 1;
+// #redo
+// should promise that it stops when begin jump down to the 2nd segment
+bool __search(vector<int> &nums, int target, int begin, int end) {
+  while (begin < end) {
+    int mid = begin + (end - begin) / 2;
+    if (nums[mid] < target) begin = mid + 1;
+    else if (nums[mid] > target) end = mid;
     else return true;
   }
   return false;
 }
 
-bool search(int A[], int n, int target) {
-  if ( n == 0 ) return false;
-  int bound = __find(A, n);
-  if ( bound >= n ) return __search(A, 0, n - 1, target);
-  bool b1 = __search(A, 0, bound - 1, target);
-  bool b2 = __search(A, bound + 1, n - 1, target);
-  bool b3 = A[bound] == target;
-  return b1 | b2 | b3;
+bool search(vector<int>& nums, int target) {
+  const int N = nums.size();
+  if (N == 0) return false;
+  int first = 0, last = N - 1;
+  while (first < last) {
+    int mid = first + (last - first) / 2;
+    if (nums[mid] < nums[first]) last = mid;
+    else if (nums[mid] == nums[first]) ++ first;
+    else {
+      first = mid + 1;
+      if (nums[mid] > nums[first]) break;
+    }
+  }
+  return __search(nums, target, 0, first) || __search(nums, target, first, N);
 }
-
