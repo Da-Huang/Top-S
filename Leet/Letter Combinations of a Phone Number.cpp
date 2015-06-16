a2e0b2710a6ad89aee48b7a04e0c5f1c4d28e3c6
Da Huang <dhuang.cn@gmail.com>
2015-06-16 19:38:15 +0800

    Letter Combinations of a Phone Number
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Letter Combinations of a Phone Number.cpp
+++ Leet/Letter Combinations of a Phone Number.cpp
@@ -1,28 +1,20 @@
-vector<string> letterCombinations(const string &digits, const string dict[], int start=0) {
-  const int N = digits.size();
-  vector<string> res;
-  if ( start == N ) {
-    res.push_back("");
-    return res;
+void __letterCombinations(string phone[], string &digits, size_t begin, string &v, vector<string> &ans) {
+  if (begin == digits.size()) {
+    ans.push_back(v);
+    return;
   }
-  vector<string> subRes = letterCombinations(digits, dict, start + 1);
-
-  const string &mapping = dict[digits[start] - '0'];
-  if ( mapping.length() == 0 ) return subRes;
-
-  for (auto &item : subRes) {
-    item.insert(item.begin(), '?');
+  for (char c : phone[digits[begin]-'0']) {
+    v.push_back(c);
+    __letterCombinations(phone, digits, begin+1, v, ans);
+    v.pop_back();
   }
-  for (auto &c : mapping) {
-    for (auto &item : subRes) {
-      item[0] = c;
-      res.push_back(item);
-    }
-  }
-  return res;
 }
 
 vector<string> letterCombinations(string digits) {
-  static const string dict[10] = {"", "", "abc", "edf", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-  return letterCombinations(digits, dict);
+  static string phone[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+  string v;
+  vector<string> ans;
+  if (digits.empty()) return ans;
+  __letterCombinations(phone, digits, 0, v, ans);
+  return ans;
 }
