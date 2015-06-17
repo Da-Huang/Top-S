e2af2c2d46a6b15cff91a454ffe3ce76b9c56e18
Da Huang <dhuang.cn@gmail.com>
2015-06-17 21:17:32 +0800

    Spiral Matrix II
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Spiral Matrix II.cpp
+++ Leet/Spiral Matrix II.cpp
@@ -1,18 +1,15 @@
-vector<vector<int> > generateMatrix(int n) {
-  vector<vector<int>> res(n, vector<int>(n));
-  if ( n == 0 ) return res;
-  int k = 1;
-  int r;
-  for (r = 0; r < n / 2; r ++) {
-    int i, j;
-    i = r;
-    for (j = r; j < n - 1 - r; j ++) res[i][j] = k ++;
-    j = n - 1 - r;
-    for (i = r; i < n - 1 - r; i ++) res[i][j] = k ++;
-    i = n - 1 - r;
-    for (j = n - 1 - r; j > r; j --) res[i][j] = k ++;
+vector<vector<int>> generateMatrix(int n) {
+  vector<vector<int>> ans(n, vector<int>(n));
+  int k = 0;
+  for (int r = 0; r < (n+1)/2; ++ r) {
+    int i = r, j;
+    for (j = r; j < n - r; ++ j) ans[i][j] = ++ k;
+    j = n - r - 1;
+    for (i = r + 1; i < n - r; ++ i) ans[i][j] = ++ k;
+    i = n - r - 1;
+    for (j = n - r - 2; j >= r; -- j) ans[i][j] = ++ k;
     j = r;
-    for (i = n - 1 - r; i > r; i --) res[i][j] = k ++;
+    for (i = n - r - 2; i > r; -- i) ans[i][j] = ++ k;
   }
-  if ( n & 1 ) res[r][r] = k;
+  return ans;
 }
