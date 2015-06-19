be264bddd1817edaf83112079d7348245d258cfb
Da Huang <dhuang.cn@gmail.com>
2015-06-19 11:57:30 +0800

    Reverse Integer
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Reverse Integer.cpp
+++ Leet/Reverse Integer.cpp
@@ -1,18 +1,12 @@
 int reverse(int x) {
-  if ( x == 0 ) return 0;
   bool isNegative = x < 0;
-  if ( x > 0 ) x = -x;
-  int res = 0;
-  while ( x ) {
-    int d = x % 10;
-    if ( res < (INT_MIN - d) / 10 ) {
-      return isNegative ? INT_MIN : INT_MAX;
-    }
-    res = res * 10 + d;
-    x = x / 10;
+  if (x > 0) x = -x;
+  int ans = 0;
+  while (x) {
+    int p = x % 10;
+    if (ans < (INT_MIN - p) / 10) return 0;
+    ans = ans * 10 + p;
+    x /= 10;
   }
-  if ( isNegative ) return res;
-  else if ( res == INT_MIN ) return INT_MAX;
-  else return -res;
+  return isNegative ? ans : ans == INT_MIN ? 0 : -ans;
 }
-
