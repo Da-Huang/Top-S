73c9a5f1a8fc71ac7c277e86a2868f0f315bdf3a
Da Huang <dhuang.cn@gmail.com>
2015-06-19 11:31:14 +0800

    Construct Binary Tree from Preorder and Inorder Traversal
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ Leet/Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -1,17 +1,32 @@
-TreeNode *__build(vector<int> &preorder, vector<int> &inorder, int preFirst, int inFirst, int n) {
-  if ( n == 0 ) return NULL;
-  TreeNode *root = new TreeNode(preorder[preFirst]);
-  int i;
-  for (i = inFirst; i < inFirst + n; i ++) {
-    if ( inorder[i] == root->val ) break;
+TreeNode *__buildTree(vector<int>& preorder, vector<int>& inorder, int preBegin, int inBegin, int N) {
+  if (N == 0) return NULL;
+  TreeNode *root = new TreeNode(preorder[preBegin]);
+  int k;
+  for (k = 0; k < N; ++ k) {
+    if (inorder[inBegin+k] == root->val) break;
   }
-  root->left = __build(preorder, inorder, preFirst + 1, inFirst, i - inFirst);
-  root->right = __build(preorder, inorder, preFirst + 1 + i - inFirst, i + 1, n - 1 - i + inFirst);
+  root->left = __buildTree(preorder, inorder, preBegin + 1, inBegin, k);
+  root->right = __buildTree(preorder, inorder, preBegin + 1 + k, inBegin + k + 1, N - k - 1);
   return root;
 }
 
-TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
-  const int N = preorder.size();
-  return __build(preorder, inorder, 0, 0, N);
+TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+  return __buildTree(preorder, inorder, 0, 0, (int) preorder.size());
 }
 
+
+// #version2
+TreeNode *__buildTree(vector<int>& preorder, vector<int>& inorder, int preBegin, int inBegin, int N, unordered_map<int, int> &inMap) {
+  if (N == 0) return NULL;
+  TreeNode *root = new TreeNode(preorder[preBegin]);
+  int k = inMap[root->val] - inBegin;
+  root->left = __buildTree(preorder, inorder, preBegin + 1, inBegin, k, inMap);
+  root->right = __buildTree(preorder, inorder, preBegin + 1 + k, inBegin + k + 1, N - k - 1, inMap);
+  return root;
+}
+
+TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+  unordered_map<int, int> inMap;
+  for (int i = 0; i < (int) inorder.size(); ++ i) inMap[inorder[i]] = i;
+  return __buildTree(preorder, inorder, 0, 0, (int) preorder.size(), inMap);
+}
