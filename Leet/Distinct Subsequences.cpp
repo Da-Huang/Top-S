804562350ea48a147f87a9d91dad634c45784f24
Da Huang <dhuang.cn@gmail.com>
2015-06-19 14:01:55 +0800

    Distinct Subsequences
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Distinct Subsequences.cpp
+++ Leet/Distinct Subsequences.cpp
@@ -1,16 +1,13 @@
-int numDistinct(string S, string T) {
-  const int M = S.length();
-  const int N = T.length();
-  int res[M + 1][N + 1];
-  res[0][0] = 1;
-  for (int i = 1; i <= M; i ++) res[i][0] = 1;
-  for (int j = 1; j <= N; j ++) res[0][j] = 0;
-  for (int i = 1; i <= M; i ++) {
-    for (int j = 1; j <= N; j ++) {
-      res[i][j] = res[i - 1][j];
-      if ( S[i - 1] == T[j - 1] ) res[i][j] += res[i - 1][j - 1];
+int numDistinct(string s, string t) {
+  int ans[s.size()+1][t.size()+1];
+  ans[0][0] = 1;
+  for (int i = 1; i <= s.size(); ++ i) ans[i][0] = 1;
+  for (int j = 1; j <= t.size(); ++ j) ans[0][j] = 0;
+  for (int i = 1; i <= s.size(); ++ i) {
+    for (int j = 1; j <= t.size(); ++ j) {
+      ans[i][j] = ans[i-1][j];
+      if (s[i-1] == t[j-1]) ans[i][j] += ans[i-1][j-1];
     }
   }
-  return res[M][N];
+  return ans[s.size()][t.size()];
 }
-
