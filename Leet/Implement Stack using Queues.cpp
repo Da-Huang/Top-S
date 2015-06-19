79dd03c0631d6f5548055a365a5c2eef3d466cbe
Da Huang <dhuang.cn@gmail.com>
2015-06-19 15:42:40 +0800

    Implement Stack using Queues
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

---
+++ Leet/Implement Stack using Queues.cpp
@@ -0,0 +1,37 @@
+class Stack {
+  queue<int> que;
+  public:
+  // Push element x onto stack.
+  void push(int x) {
+    que.push(x);
+  }
+
+  // Removes the element on top of the stack.
+  void pop() {
+    queue<int> queBuffer;
+    while (que.size() > 1) {
+      queBuffer.push(que.front());
+      que.pop();
+    }
+    que = move(queBuffer);
+  }
+
+  // Get the top element.
+  int top() {
+    queue<int> queBuffer;
+    while (que.size() > 1) {
+      queBuffer.push(que.front());
+      que.pop();
+    }
+    int ans = que.front();
+    queBuffer.push(que.front());
+    que.pop();
+    que = move(queBuffer);
+    return ans;
+  }
+
+  // Return whether the stack is empty.
+  bool empty() {
+    return que.empty();
+  }
+};
