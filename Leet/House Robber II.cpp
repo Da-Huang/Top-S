10a80464aa8ccc7b9af18e837446748df48eb77c
Da Huang <dhuang.cn@gmail.com>
2015-06-19 10:34:02 +0800

    House Robber II
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/House Robber II.cpp
+++ Leet/House Robber II.cpp
@@ -1,49 +1,20 @@
 // #redo
-// TODO: do it in O(1) space
+int __rob(vector<int> &nums, int begin, int end) {
+  int ans = 0;
+  int pre0 = 0, pre1 = 0;
+  for (int i = begin; i < end; ++ i) {
+    int ans0 = max(pre0, pre1);
+    int ans1 = pre0 + nums[i];
+    ans = max({ans0, ans1, ans});
+    pre0 = ans0;
+    pre1 = ans1;
+  }
+  return ans;
+}
+
 int rob(vector<int>& nums) {
   const int N = nums.size();
   if (N == 0) return 0;
   if (N == 1) return nums[0];
-
-  int maxAns = 0;
-  int rans[N];
-  int pre0 = 0, pre1 = 0;
-  for (int i = N - 1; i > 0; -- i) {
-    int ans0 = max(pre0, pre1);
-    int ans1 = pre0 + nums[i];
-    pre0 = ans0;
-    pre1 = ans1;
-    rans[i] = max(ans0, ans1);
-  }
-
-  pre0 = 0, pre1 = 0;
-  for (int i = 1; i < N; ++ i) {
-    int ans0 = max(pre0, pre1);
-    int ans1 = pre0 + nums[i];
-    pre0 = ans0;
-    pre1 = ans1;
-    int ans = max(ans0, ans1) - (i == N - 1 ? 0 : rans[i+1]);
-    maxAns = max(maxAns, ans);
-  }
-
-  pre0 = 0, pre1 = 0;
-  for (int i = N - 2; i >= 0; -- i) {
-    int ans0 = max(pre0, pre1);
-    int ans1 = pre0 + nums[i];
-    pre0 = ans0;
-    pre1 = ans1;
-    rans[i] = max(ans0, ans1);
-  }
-
-  pre0 = 0, pre1 = 0;
-  for (int i = 0; i < N - 1; ++ i) {
-    int ans0 = max(pre0, pre1);
-    int ans1 = pre0 + nums[i];
-    pre0 = ans0;
-    pre1 = ans1;
-    int ans = max(ans0, ans1) - (i == N - 2 ? 0 : rans[i+1]);
-    maxAns = max(maxAns, ans);
-  }
-
-  return maxAns;
+  return max(__rob(nums, 0, N - 1), __rob(nums, 1, N));
 }
