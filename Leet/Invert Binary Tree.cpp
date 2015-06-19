32f9814bb281b7950faf0190da7fb9a346e76d9e
Da Huang <dhuang.cn@gmail.com>
2015-06-19 14:06:47 +0800

    Invert Binary Tree
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

---
+++ Leet/Invert Binary Tree.cpp
@@ -0,0 +1,7 @@
+TreeNode* invertTree(TreeNode* root) {
+  if (root == NULL) return NULL;
+  root->left = invertTree(root->left);
+  root->right = invertTree(root->right);
+  swap(root->left, root->right);
+  return root;
+}
