ba664c46fed9b9ca156c20207741a3ac6f9a977c
Da Huang <dhuang.cn@gmail.com>
2015-06-20 22:10:29 +0800

    Path Sum
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Path Sum.cpp
+++ Leet/Path Sum.cpp
@@ -1,15 +1,7 @@
-// Redo
-bool hasPathSum(TreeNode *root, int sum) {
-  if ( root == NULL ) return false;
-  if ( root->left == NULL && root->right == NULL ) {
-    return root->val == sum;
-  }
-  if ( root->left ) {
-    if ( hasPathSum(root->left, sum - root->val) ) return true;
-  }
-  if ( root->right ) {
-    if ( hasPathSum(root->right, sum - root->val) ) return true;
-  }
+bool hasPathSum(TreeNode* root, int sum) {
+  if (root == NULL) return false;
+  if (root->left == NULL && root->right == NULL) return sum == root->val;
+  if (hasPathSum(root->left, sum - root->val)) return true;
+  if (hasPathSum(root->right, sum - root->val)) return true;
   return false;
 }
-
