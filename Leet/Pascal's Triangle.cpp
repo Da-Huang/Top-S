0005ceb7d36f649d85954c0bdf478e47467f9a99
Da Huang <dhuang.cn@gmail.com>
2015-06-20 21:54:43 +0800

    Pascal's Triangle
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Pascal's Triangle.cpp
+++ Leet/Pascal's Triangle.cpp
@@ -1,14 +1,9 @@
-vector<vector<int> > generate(int numRows) {
-  vector<vector<int>> res;
-  if ( numRows == 0 ) return res;
-  res.push_back(vector<int>(1, 1));
-  for (int i = 1; i < numRows; i ++) {
-    res.push_back(vector<int>(i + 1));
-    res[i][0] = 1;
-    res[i][i] = 1;
-    for (int j = 1; j < i; j ++) {
-      res[i][j] = res[i - 1][j - 1] + res[i - 1][j];
-    }
+vector<vector<int>> generate(int numRows) {
+  vector<vector<int>> ans;
+  if (numRows == 0) return ans;
+  for (int i = 0; i < numRows; ++ i) {
+    ans.push_back(vector<int>(i + 1, 1));
+    for (int j = 1; j < i; ++ j) ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
   }
-  return res;
+  return ans;
 }
