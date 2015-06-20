edb97e1c2b0d934512a3470c616cd23ed6085269
Da Huang <dhuang.cn@gmail.com>
2015-06-20 21:26:54 +0800

    Sum Root to Leaf Numbers
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Sum Root to Leaf Numbers.cpp
+++ Leet/Sum Root to Leaf Numbers.cpp
@@ -1,18 +1,13 @@
-int __sumNumbers(TreeNode *root, int mul) {
-  if ( root->left == NULL && root->right == NULL ) {
-    return mul * 10 + root->val;
-  }
-  int res = 0;
-  if ( root->left ) {
-    res += __sumNumbers(root->left, mul * 10 + root->val);
-  }
-  if ( root->right ) {
-    res += __sumNumbers(root->right, mul * 10 + root->val);
-  }
-  return res;
+int __sumNumbers(TreeNode *root, int base) {
+  if (root == NULL) return 0;
+  base = base * 10 + root->val;
+  if (root->left == NULL && root->right == NULL) return base;
+  int ans = 0;
+  if (root->left) ans += __sumNumbers(root->left, base);
+  if (root->right) ans += __sumNumbers(root->right, base);
+  return ans;
 }
 
-int sumNumbers(TreeNode *root) {
-  if ( root == NULL ) return 0;
+int sumNumbers(TreeNode* root) {
   return __sumNumbers(root, 0);
 }
