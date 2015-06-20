9d2442b9315e46271f5c5690b729ccdb37e67220
Da Huang <dhuang.cn@gmail.com>
2015-06-20 22:11:32 +0800

    Path Sum II
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Path Sum II.cpp
+++ Leet/Path Sum II.cpp
@@ -24,3 +24,27 @@ vector<vector<int> > pathSum(TreeNode *root, int sum) {
   return res;
 }
 
+
+// #redo
+// #version2
+void __pathSum(TreeNode* root, int sum, vector<int> &v, vector<vector<int>> &ans) {
+  if (root->left == NULL && root->right == NULL) {
+    if (sum == root->val) {
+      ans.push_back(v);
+      ans.back().push_back(root->val);
+    }
+    return;
+  }
+  v.push_back(root->val);
+  if (root->left) __pathSum(root->left, sum - root->val, v, ans);
+  if (root->right) __pathSum(root->right, sum - root->val, v, ans);
+  v.pop_back();
+}
+
+vector<vector<int>> pathSum(TreeNode* root, int sum) {
+  vector<vector<int>> ans;
+  if (root == NULL) return ans;
+  vector<int> v;
+  __pathSum(root, sum, v, ans);
+  return ans;
+}
