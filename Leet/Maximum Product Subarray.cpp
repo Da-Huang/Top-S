03a56f430fccf71d4ed29dba73cfc6ecd16a7eff
Da Huang <dhuang.cn@gmail.com>
2015-06-20 10:30:09 +0800

    Maximum Product Subarray
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Maximum Product Subarray.cpp
+++ Leet/Maximum Product Subarray.cpp
@@ -1,15 +1,14 @@
-int maxProduct(int A[], int n) {
-  if ( n == 0 ) return 0;
-  int minRes = A[0];
-  int maxRes = A[0];
-  int res = A[0];
-  for (int i = 1; i < n; i ++) {
-    int newMinRes = min({minRes * A[i], maxRes * A[i], A[i]});
-    int newMaxRes = max({minRes * A[i], maxRes * A[i], A[i]});
-    minRes = newMinRes;
-    maxRes = newMaxRes;
-    res = max(res, maxRes);
+int maxProduct(vector<int>& nums) {
+  const int N = nums.size();
+  if (N == 0) return 0;
+  int maxValue = nums[0];
+  int minValue = nums[0];
+  int ans = nums[0];
+  for (int i = 1; i < N; ++ i) {
+    int tmp = max({nums[i], minValue * nums[i], maxValue * nums[i]});
+    minValue = min({nums[i], minValue * nums[i], maxValue * nums[i]});
+    maxValue = tmp;
+    ans = max(ans, maxValue);
   }
-  return res;
+  return ans;
 }
-
