313cd7139f629afdf53f748395fac6437ccd986e
Da Huang <dhuang.cn@gmail.com>
2015-06-20 20:42:40 +0800

    Binary Tree Maximum Path Sum
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Binary Tree Maximum Path Sum.cpp
+++ Leet/Binary Tree Maximum Path Sum.cpp
@@ -1,26 +1,21 @@
-// Redo
+// #redo
 pair<int, int> __maxPathSum(TreeNode *root) {
-  if ( root->left == NULL && root->right == NULL )
-    return make_pair(root->val, root->val);
-  int l1 = INT_MIN, l2 = INT_MIN;
-  int r1 = INT_MIN, r2 = INT_MIN;
-  if ( root->left ) {
-    auto l = __maxPathSum(root->left);
-    l1 = l.first;
-    l2 = l.second;
+  if (root->left == NULL && root->right == NULL) return make_pair(root->val, root->val);
+  pair<int, int> leftAns, rightAns;
+  int leftSingle, rightSingle;
+  if (root->left) {
+    leftAns = __maxPathSum(root->left);
+    leftSingle = root->val + max(leftAns.second, 0);
+    if (root->right == NULL) return make_pair(max(leftSingle, leftAns.first), leftSingle);
   }
-  if ( root->right ) {
-    auto r = __maxPathSum(root->right);
-    r1 = r.first;
-    r2 = r.second;
+  if (root->right) {
+    rightAns = __maxPathSum(root->right);
+    rightSingle = root->val + max(rightAns.second, 0);
+    if (root->left == NULL) return make_pair(max(rightSingle, rightAns.first), rightSingle);
   }
-  int res1 = max({l1, r1, 0}) + root->val;
-  int res2 = max({res1, l1, r1, l2, r2, max(l1, 0) + max(r1, 0) + root->val});
-  return make_pair(res1, res2);
+  return make_pair(max({leftAns.first, rightAns.first, leftSingle + rightSingle - root->val}), max(leftSingle, rightSingle));
 }
 
-int maxPathSum(TreeNode *root) {
-  if ( root == NULL ) return 0;
-  return __maxPathSum(root).second;
+int maxPathSum(TreeNode* root) {
+  return __maxPathSum(root).first;
 }
-
