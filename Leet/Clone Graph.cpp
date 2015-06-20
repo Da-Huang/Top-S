793260c707afee4ffa6bcdf5eb0c845555374bf3
Da Huang <dhuang.cn@gmail.com>
2015-06-20 18:04:02 +0800

    Clone Graph
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Clone Graph.cpp
+++ Leet/Clone Graph.cpp
@@ -1,19 +1,37 @@
+// #redo
+UndirectedGraphNode *__cloneGraph(UndirectedGraphNode *node, unordered_map<UndirectedGraphNode*, UndirectedGraphNode*> &MAP) {
+  if (node == NULL) return NULL;
+  if (MAP.find(node) != MAP.end()) return MAP[node];
+  UndirectedGraphNode *newNode = MAP[node] = new UndirectedGraphNode(node->label);
+  for (UndirectedGraphNode *neighbor : node->neighbors) {
+    newNode->neighbors.push_back(__cloneGraph(neighbor, MAP));
+  }
+  return newNode;
+}
+
 UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
-  if ( node == NULL ) return NULL;
-  unordered_map<UndirectedGraphNode*, UndirectedGraphNode*> mapping;
-  stack<UndirectedGraphNode*> nodeStack;
-  nodeStack.push(node);
-  mapping[node] = new UndirectedGraphNode(node->label);
-  while ( !nodeStack.empty() ) {
-    UndirectedGraphNode *cur = nodeStack.top();
-    nodeStack.pop();
-    for (auto &neighbor : cur->neighbors) {
-      if ( mapping.find(neighbor) == mapping.end() ) {
-        mapping[neighbor] = new UndirectedGraphNode(neighbor->label);
-        nodeStack.push(neighbor);
+  unordered_map<UndirectedGraphNode*, UndirectedGraphNode*> MAP;
+  return __cloneGraph(node, MAP);
+}
+
+
+// #version2
+UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+  if (node == NULL) return NULL;
+  unordered_map<UndirectedGraphNode*, UndirectedGraphNode*> MAP;
+  stack<UndirectedGraphNode*> stk;
+  MAP[node] = new UndirectedGraphNode(node->label);
+  stk.push(node);
+  while (!stk.empty()) {
+    UndirectedGraphNode *node = stk.top();
+    stk.pop();
+    for (UndirectedGraphNode *neighbor : node->neighbors) {
+      if (MAP.find(neighbor) == MAP.end()) {
+        stk.push(neighbor);
+        MAP[neighbor] = new UndirectedGraphNode(neighbor->label);
       }
-      mapping[cur]->neighbors.push_back(mapping[neighbor]);
+      MAP[node]->neighbors.push_back(MAP[neighbor]);
     }
   }
-  return mapping[node];
+  return MAP[node];
 }
