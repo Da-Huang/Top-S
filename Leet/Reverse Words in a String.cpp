ab486a2f585cc44af89fcfa1f1c0952e8d9332d2
Da Huang <dhuang.cn@gmail.com>
2015-06-20 10:55:43 +0800

    Reverse Words in a String
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Reverse Words in a String.cpp
+++ Leet/Reverse Words in a String.cpp
@@ -1,17 +1,19 @@
+// #redo
 void reverseWords(string &s) {
-  const int N = s.length();
-  int len = 0, begin = 0;
-  while ( begin < N && s[begin] == ' ' ) begin ++;
-  while ( begin < N ) {
+  const int N = s.size();
+  int k = 0;
+  int begin = 0;
+  while (begin < N && s[begin] == ' ') ++ begin;
+  while (begin < N) {
     int end = begin + 1;
-    while ( end < N && s[end] != ' ' ) end ++;
+    while (end < N && s[end] != ' ') ++ end;
     reverse(s.begin() + begin, s.begin() + end);
-    copy(s.begin() + begin, s.begin() + end, s.begin() + len);
-    len += end - begin;
-    while ( end < N && s[end] == ' ' ) end ++;
+    copy(s.begin() + begin, s.begin() + end, s.begin() + k);
+    k += end - begin;
     begin = end;
-    if ( begin < N ) s[len ++] = ' ';
-  }
-  s.resize(len);
+    while (begin < N && s[begin] == ' ') ++ begin;
+    if (begin < N) s[k ++] = ' ';
+  };
+  s.resize(k);
   reverse(s.begin(), s.end());
 }
