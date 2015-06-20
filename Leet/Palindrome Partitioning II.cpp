d7d3b40c6fdfd656edb9a770984b55f06c7051e3
Da Huang <dhuang.cn@gmail.com>
2015-06-20 19:03:59 +0800

    Palindrome Partitioning II
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Palindrome Partitioning II.cpp
+++ Leet/Palindrome Partitioning II.cpp
@@ -1,31 +1,26 @@
+// #redo
 int minCut(string s) {
-  const int N = s.length();
-  if ( N == 0 ) return 0;
-  vector<vector<bool>> palin(N, vector<bool>(N));
-  palin[0][0] = true;
-  for (int i = 1; i < N; i ++) {
-    palin[i][i] = true;
-    palin[i - 1][i] = s[i - 1] == s[i];
-  }
-  for (int i = N - 3; i >= 0; i --) {
-    for (int j = i + 2; j < N; j ++) {
-      palin[i][j] = palin[i + 1][j - 1] && s[i] == s[j];
+  const int N = s.size();
+  if (N <= 1) return 0;
+  vector<vector<bool>> isPalin(N, vector<bool>(N));
+  isPalin[N-1][N-1] = true;
+  for (int i = N - 2; i >= 0; -- i) {
+    isPalin[i][i] = true;
+    isPalin[i][i+1] = s[i] == s[i+1];
+    for (int j = i + 2; j < N; ++ j) {
+      isPalin[i][j] = isPalin[i+1][j-1] && s[i] == s[j];
     }
   }
-  int res[N];
-  res[0] = 0;
-  for (int i = 1; i < N; i ++) {
-    if ( palin[0][i] ) res[i] = 0;
+  int ans[N];
+  ans[0] = 0;
+  for (int i = 1; i < N; ++ i) {
+    if (isPalin[0][i]) ans[i] = 0;
     else {
-      int can = i;
-      for (int j = 0; j < i; j ++) {
-        if ( palin[j + 1][i] ) {
-          can = min(can, res[j] + 1);
-        }
+      ans[i] = i;
+      for (int j = 1; j <= i; ++ j) {
+        if (isPalin[j][i]) ans[i] = min(ans[i], ans[j - 1] + 1);
       }
-      res[i] = can;
     }
   }
-  return res[N - 1];
+  return ans[N - 1];
 }
-
