50a3fce36198ad8ca790e1c87b627775801ec444
Da Huang <dhuang.cn@gmail.com>
2015-06-20 16:33:14 +0800

    Copy List with Random Pointer.cpp
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Copy List with Random Pointer.cpp
+++ Leet/Copy List with Random Pointer.cpp
@@ -1,14 +1,14 @@
 RandomListNode *copyRandomList(RandomListNode *head) {
-  unordered_map<RandomListNode*, RandomListNode*> mapping;
+  unordered_map<RandomListNode*, RandomListNode*> MAP;
   RandomListNode HEAD(0);
   RandomListNode *tail = &HEAD;
   for (RandomListNode *ptr = head; ptr; ptr = ptr->next) {
-    tail->next = mapping[ptr] = new RandomListNode(ptr->label);
+    tail->next = MAP[ptr] = new RandomListNode(ptr->label);
     tail = tail->next;
   }
-  for (RandomListNode *ptr = head, *q = HEAD.next; ptr; ptr = ptr->next, q = q->next) {
-    if ( ptr->random ) {
-      q->random = mapping[ptr->random];
+  for (RandomListNode *ptr = head; ptr; ptr = ptr->next) {
+    if (ptr->random) {
+      MAP[ptr]->random = MAP[ptr->random];
     }
   }
   return HEAD.next;
