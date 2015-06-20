ab12a08b8224fc7f77b581f7bc89a9e88badea9a
Da Huang <dhuang.cn@gmail.com>
2015-06-20 11:47:33 +0800

    Sort List
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Sort List.cpp
+++ Leet/Sort List.cpp
@@ -1,59 +1,44 @@
-// Return next tail
-ListNode *merge(ListNode *HEAD, ListNode *MID, ListNode *TAIL) {
-  ListNode *nextHead = TAIL->next;
-  ListNode *head1 = HEAD->next;
-  ListNode *head2 = MID->next;
-  ListNode *tail1 = MID;
-  ListNode *tail2 = TAIL;
-  tail1->next = NULL;
-  tail2->next = NULL;
-  ListNode *tail = HEAD;
-  while ( head1 && head2 ) {
-    if ( head1->val < head2->val ) {
-      tail->next = head1;
-      tail = head1;
-      head1 = head1->next;
-
-    } else {
-      tail->next = head2;
-      tail = head2;
-      head2 = head2->next;
-    }
-  }
-  if ( head1 ) {
-    tail->next = head1;
-    tail = tail1;
-  }
-  if ( head2 ) {
-    tail->next = head2;
-    tail = tail2;
-  }
-  tail->next = nextHead;
-  return tail;
-}
-
-ListNode *sortList(ListNode *head) {
+ListNode* sortList(ListNode* head) {
+  int n = 1;
   ListNode HEAD(0);
   HEAD.next = head;
-  int n = 1;
-  while ( true ) {
-    ListNode *subHead = &HEAD;
-    while ( true ) {
-      ListNode *tail1 = subHead;
-      for (int i = 0; i < n; i ++) {
-        if ( tail1->next == NULL ) break;
-        tail1 = tail1->next;
-      }
-      if ( tail1->next == NULL ) break;
-      ListNode *tail2 = tail1;
-      for (int i = 0; i < n; i ++) {
-        if ( tail2->next == NULL ) break;
-        tail2 = tail2->next;
-      }
-      subHead = merge(subHead, tail1, tail2);
-    }
-    n <<= 1;
-    if ( subHead == &HEAD ) return HEAD.next;
-  }
-}
+  ListNode *tail = &HEAD;
+  while (true) {
+    ListNode *tail1 = tail;
+    for (int i = 0; i < n && tail1->next; ++ i) tail1 = tail1->next;
+    if (tail1->next == NULL && tail == &HEAD) break;
 
+    ListNode *tail2 = tail1;
+    for (int i = 0; i < n && tail2->next; ++ i) tail2 = tail2->next;
+
+    ListNode *nextHead = tail2->next;
+    ListNode *head1 = tail->next, *head2 = tail1->next;
+    tail1->next = NULL, tail2->next = NULL;
+    while (head1 && head2) {
+      ListNode *node;
+      if (head1->val <= head2->val) {
+        node = head1;
+        head1 = head1->next;
+      } else {
+        node = head2;
+        head2 = head2->next;
+      }
+      tail->next = node;
+      tail = node;
+    }
+    if (head1) {
+      tail->next = head1;
+      tail = tail1;
+    }
+    if (head2) {
+      tail->next = head2;
+      tail = tail2;
+    }
+    tail->next = nextHead;
+    if (nextHead == NULL) {
+      tail = &HEAD;
+      n <<= 1;
+    }
+  }
+  return HEAD.next;
+}
