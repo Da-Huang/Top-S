21e5920be35be8ed2f42651cf34de3efe281b187
Da Huang <dhuang.cn@gmail.com>
2015-06-21 07:38:10 +0800

    Interleaving String
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Interleaving String.cpp
+++ Leet/Interleaving String.cpp
@@ -1,20 +1,16 @@
+// #redo
 bool isInterleave(string s1, string s2, string s3) {
-  const int M = s1.length();
-  const int N = s2.length();
-  const int T = s3.length();
-  if ( M + N != T ) return false;
-  vector<vector<bool>> res(M + 1, vector<bool>(N + 1));
-  res[0][0] = true;
-  for (int j = 1; j <= N; j ++)
-    res[0][j] = res[0][j - 1] && s2[j - 1] == s3[j - 1];
-  for (int i = 1; i <= M; i ++)
-    res[i][0] = res[i - 1][0] && s1[i - 1] == s3[i - 1];
-  for (int i = 1; i <= M; i ++) {
-    for (int j = 1; j <= N; j ++) {
-      res[i][j] = res[i - 1][j] && s1[i - 1] == s3[i + j - 1] ||
-        res[i][j - 1] && s2[j - 1] == s3[i + j - 1];
+  const int s1n = s1.size();
+  const int s2n = s2.size();
+  if (s1n + s2n != (int) s3.size()) return false;
+  vector<vector<bool>> ans(s1n+1, vector<bool>(s2n+1));
+  ans[0][0] = true;
+  for (int j = 1; j <= s2n; ++ j) ans[0][j] = ans[0][j-1] && s2[j-1] == s3[j-1];
+  for (int i = 1; i <= s1n; ++ i) {
+    ans[i][0] = ans[i-1][0] && s1[i-1] == s3[i-1];
+    for (int j = 1; j <= s2n; ++ j) {
+      ans[i][j] = (ans[i-1][j] && s1[i-1] == s3[i+j-1]) || (ans[i][j-1] && s2[j-1] == s3[i+j-1]);
     }
   }
-  return res[M][N];
+  return ans[s1n][s2n];
 }
-
