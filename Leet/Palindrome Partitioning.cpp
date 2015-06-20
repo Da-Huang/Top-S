c52027470551a9d787aba629f249c0d0e29be70a
Da Huang <dhuang.cn@gmail.com>
2015-06-20 19:15:51 +0800

    Palindrome Partitioning.cpp
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Palindrome Partitioning.cpp
+++ Leet/Palindrome Partitioning.cpp
@@ -1,44 +1,36 @@
-vector<vector<string>> __partition(string &s, vector<vector<bool>> palin, int begin, 
-    vector<vector<string>> cache[], vector<bool> &cacheValid) {
-  vector<vector<string>> res;
-  const int N = s.length();
-  if ( begin == N ) {
-    res.push_back(vector<string>());
-    return res;
+bool __partition(string &s, int i, vector<vector<bool>> &isPalin, vector<bool> &able, vector<string> &v, vector<vector<string>> &ans) {
+  if (i == (int) s.size()) {
+    ans.push_back(v);
+    return true;
   }
-  if ( cacheValid[begin] ) return cache[begin];
+  if (!able[i]) return false;
 
-  for (int i = begin; i < N; i ++) {
-    if ( palin[begin][i] ) {
-      string part = s.substr(begin, i + 1 - begin);
-      vector<vector<string>> subRes = __partition(s, palin, i + 1, cache, cacheValid);
-      for (auto &v : subRes) {
-        v.insert(v.begin(), part);
-        res.push_back(v);
-      }
+  bool mayAble = false;
+  for (int j = i; j < (int) s.size(); ++ j) {
+    if (isPalin[i][j]) {
+      v.push_back(s.substr(i, j + 1 - i));
+      mayAble |= __partition(s, j + 1, isPalin, able, v, ans);
+      v.pop_back();
     }
   }
-  cacheValid[begin] = true;
-  return cache[begin] = res;
+  return able[i] = mayAble;
 }
 
 vector<vector<string>> partition(string s) {
-  const int N = s.length();
-  if ( N == 0 ) return vector<vector<string>>();
-
-  vector<vector<bool>> palin(N, vector<bool>(N));
-  palin[0][0] = true;
-  for (int i = 1; i < N; i ++) {
-    palin[i][i] = true;
-    palin[i - 1][i] = s[i - 1] == s[i];
-  }
-  for (int i = N - 3; i >= 0; i --) {
-    for (int j = i + 2; j < N; j ++) {
-      palin[i][j] = palin[i + 1][j - 1] && s[i] == s[j];
+  vector<vector<string>> ans;
+  const int N = s.size();
+  if (N == 0) return ans;
+  vector<vector<bool>> isPalin(N, vector<bool>(N));
+  isPalin[N-1][N-1] = true;
+  for (int i = N - 2; i >= 0; -- i) {
+    isPalin[i][i] = true;
+    isPalin[i][i+1] = s[i] == s[i+1];
+    for (int j = i + 2; j < N; ++ j) {
+      isPalin[i][j] = isPalin[i+1][j-1] && s[i] == s[j];
     }
   }
-  vector<vector<string>> cache[N];
-  vector<bool> cacheValid(N, false);
-  return __partition(s, palin, 0, cache, cacheValid);
+  vector<string> v;
+  vector<bool> able(N, true);
+  __partition(s, 0, isPalin, able, v, ans);
+  return ans;
 }
-
