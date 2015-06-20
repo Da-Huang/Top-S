d77344e66883dec889151c0fa85a060f97f8e426
Da Huang <dhuang.cn@gmail.com>
2015-06-20 21:30:53 +0800

    Pascal's Triangle II
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Pascal's Triangle II.cpp
+++ Leet/Pascal's Triangle II.cpp
@@ -1,11 +1,9 @@
 vector<int> getRow(int rowIndex) {
-  vector<int> res(rowIndex + 1);
-  res[0] = 1;
-  for (int i = 1; i <= rowIndex; i ++) {
-    res[i] = 1;
-    for (int j = i - 1; j > 0; j --)
-      res[j] += res[j - 1];
+  vector<int> ans(rowIndex + 1);
+  ans[0] = 1;
+  for (int i = 1; i <= rowIndex; ++ i) {
+    ans[i] = 1;
+    for (int j = i - 1; j > 0; -- j) ans[j] += ans[j-1];
   }
-  return res;
+  return ans;
 }
-
