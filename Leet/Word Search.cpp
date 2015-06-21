a67977f2b3e7a48dc7bbee1a4aff9181486e473e
Da Huang <dhuang.cn@gmail.com>
2015-06-21 10:45:35 +0800

    Word Search
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Word Search.cpp
+++ Leet/Word Search.cpp
@@ -1,38 +1,31 @@
-// Redo
-bool __exist(vector<vector<char>> &board, const string &word, vector<vector<bool>> &visited, int begin, int i, int j) {
-  const int L = word.length();
-  if ( begin == L ) return true;
-  if ( word[begin] != board[i][j] ) return false;
-  if ( begin + 1 == L ) return true;
-
+bool __exist(vector<vector<char>>& board, string &word, vector<vector<bool>> &visited, int i, int j, int k) {
+  if (k == (int) word.size()) return true;
+  if (i < 0 || i >= (int) board.size()) return false;
+  if (j < 0 || j >= (int) board[0].size()) return false;
+  if (visited[i][j]) return false;
+  if (board[i][j] != word[k]) return false;
   visited[i][j] = true;
-  const int M = board.size();
-  const int N = board[0].size();
-  if ( i > 0 && !visited[i - 1][j] ) {
-    if ( __exist(board, word, visited, begin + 1, i - 1, j) ) return true;
-  }
-  if ( j > 0 && !visited[i][j - 1] ) {
-    if ( __exist(board, word, visited, begin + 1, i, j - 1) ) return true;
-  }
-  if ( i < M - 1 && !visited[i + 1][j] ) {
-    if ( __exist(board, word, visited, begin + 1, i + 1, j) ) return true;
-  }
-  if ( j < N - 1 && !visited[i][j + 1] ) {
-    if ( __exist(board, word, visited, begin + 1, i, j + 1) ) return true;
-  }
+  if (
+      __exist(board, word, visited, i - 1, j, k + 1) ||
+      __exist(board, word, visited, i, j - 1, k + 1) ||
+      __exist(board, word, visited, i + 1, j, k + 1) ||
+      __exist(board, word, visited, i, j + 1, k + 1)) return true;
   visited[i][j] = false;
   return false;
 }
 
-bool exist(vector<vector<char> > &board, string word) {
+bool exist(vector<vector<char>>& board, string word) {
+  if (word.size() == 0) return true;
   const int M = board.size();
+  if (M == 0) return false;
   const int N = board[0].size();
-  vector<vector<bool>> visited(M, vector<bool>(N, false));
-  for (int i = 0; i < M; i ++) {
-    for (int j = 0; j < N; j ++) {
-      if ( __exist(board, word, visited, 0, i, j) ) return true;
+  if (N == 0) return false;
+
+  vector<vector<bool>> visited(board.size(), vector<bool>(board[0].size(), false));
+  for (int i = 0; i < M; ++ i) {
+    for (int j = 0; j < N; ++ j) {
+      if (__exist(board, word, visited, i, j, 0)) return true;
     }
   }
   return false;
 }
-
