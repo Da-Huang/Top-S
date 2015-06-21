b54a3de06db38e33ec3a9dc627573e4ec31b52dc
Da Huang <dhuang.cn@gmail.com>
2015-06-21 10:16:04 +0800

    Remove Duplicates from Sorted List II
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Remove Duplicates from Sorted List II.cpp
+++ Leet/Remove Duplicates from Sorted List II.cpp
@@ -1,20 +1,19 @@
-ListNode *deleteDuplicates(ListNode *head) {
+ListNode* deleteDuplicates(ListNode* head) {
   ListNode HEAD(0);
   HEAD.next = head;
   ListNode *ptr = &HEAD;
-  while ( ptr->next ) {
+  while (ptr->next) {
     ListNode *q = ptr->next;
-    if ( q->next && q->val == q->next->val ) {
-      ptr->next = q->next;
-      while ( ptr->next && ptr->next->val == q->val ) {
-        ListNode *node = ptr->next;
-        ptr->next = node->next;
+    if (q->next && q->next->val == ptr->next->val) {
+      while (q->next && q->next->val == ptr->next->val) {
+        ListNode *node = q->next;
+        q->next = node->next;
         delete node;
       }
-      delete q;
-    } else {
-      ptr = ptr->next;
-    }
+      ListNode *node = ptr->next;
+      ptr->next = node->next;
+      delete node;
+    } else ptr = ptr->next;
   }
   return HEAD.next;
 }
