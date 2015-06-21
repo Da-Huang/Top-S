332433f498e4f57dd42f8505e0f17f4aadf17ef1
Da Huang <dhuang.cn@gmail.com>
2015-06-21 15:42:38 +0800

    Remove Nth Node From End of List
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Remove Nth Node From End of List.cpp
+++ Leet/Remove Nth Node From End of List.cpp
@@ -1,15 +1,15 @@
-// Method 1
-ListNode *removeNthFromEnd(ListNode *head, int n) {
+ListNode* removeNthFromEnd(ListNode* head, int n) {
+  if (head == NULL) return NULL;
   ListNode HEAD(0);
   HEAD.next = head;
-  ListNode *ptr = &HEAD;
-  for (int i = 0; i < n; i ++) {
-    ptr = ptr->next;
-    if ( ptr == NULL ) return NULL;
+  ListNode *p = &HEAD;
+  for (int i = 0; i < n; ++ i) {
+    if (p->next == NULL) return head;
+    p = p->next;
   }
   ListNode *q = &HEAD;
-  while ( ptr->next ) {
-    ptr = ptr->next;
+  while (p->next) {
+    p = p->next;
     q = q->next;
   }
   ListNode *node = q->next;
@@ -18,16 +18,16 @@ ListNode *removeNthFromEnd(ListNode *head, int n) {
   return HEAD.next;
 }
 
-// Method 2
-ListNode *removeNthFromEnd(ListNode *head, int n) {
+
+// #solution2
+ListNode* removeNthFromEnd(ListNode* head, int n) {
+  if (head == NULL) return NULL;
   ListNode HEAD(0);
   HEAD.next = head;
   ListNode *cache[n];
-  int i = 0;
-  for (ListNode *ptr = &HEAD; ptr->next; ptr = ptr->next) {
-    cache[i ++ % n] = ptr;
-  }
-  ListNode *ptr = cache[i % n];
+  int count = 0;
+  for (ListNode *ptr = &HEAD; ptr->next; ptr = ptr->next) cache[(count ++)%n] = ptr;
+  ListNode *ptr = cache[(count - n)%n];
   ListNode *node = ptr->next;
   ptr->next = node->next;
   delete node;
