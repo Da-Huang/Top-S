396cef0a7194a7de339b02bd3e961032c2471b9f
Da Huang <dhuang.cn@gmail.com>
2015-06-21 21:14:30 +0800

    Implement strStr()
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Implement strStr().cpp
+++ Leet/Implement strStr().cpp
@@ -1,28 +1,27 @@
-char *strStr(char *haystack, char *needle) {
-  const int H = strlen(haystack);
-  const int N = strlen(needle);
-  int next[N];
-  if ( N == 0 ) return haystack;
+int strStr(string haystack, string needle) {
+  const int M = needle.size();
+  if (M == 0) return 0;
+  int next[M];
   next[0] = -1;
-  int i = 1;
-  int j = next[0];
-  while ( i < N ) {
-    if ( j < 0 ) next[i ++] = ++ j;
-    else if ( needle[i - 1] == needle[j] ) next[i ++] = ++ j;
+  int i = 1, j = -1;
+  while (i < M) {
+    if (j < 0 || needle[i-1] == needle[j]) next[i ++] = ++ j;
     else j = next[j];
   }
 
-  i = 0; j = 0;
-  while ( i < H && j < N ) {
-    if ( j < 0 ) {
-      i ++; j ++;
-
-    } else if ( haystack[i] == needle[j] ) {
-      i ++; j ++;
-
-    } else j = next[j]; 
+  const int N = haystack.size();
+  i = 0, j = 0;
+  while (i < N) {
+    if (j < 0) {
+      j = 0;
+      ++ i;
+    } else if (haystack[i] == needle[j]) {
+      ++ i;
+      ++ j;
+      if (j == M) return i - M;
+    } else {
+      j = next[j];
+    }
   }
-  if ( j == N ) return haystack + i - N;
-  else return NULL;
+  return -1;
 }
-
