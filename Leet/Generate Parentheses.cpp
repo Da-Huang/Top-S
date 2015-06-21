0c7ba1967e8610827a3bf4ed907fe7d8747ef236
Da Huang <dhuang.cn@gmail.com>
2015-06-21 20:50:59 +0800

    Generate Parentheses
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Generate Parentheses.cpp
+++ Leet/Generate Parentheses.cpp
@@ -20,3 +20,75 @@ vector<string> generateParenthesis(int n) {
   return res[n];
 }
 
+
+// #version2
+vector<string> generateParenthesis(int n) {
+  vector<string> ans;
+  if (n == 0) return ans;
+  if (n == 1) {
+    ans.push_back("()");
+    return ans;
+  }
+  auto subAns = generateParenthesis(n - 1);
+  for (string &item : subAns) {
+    ans.push_back(item + "()");
+    ans.push_back("(" + item + ")");
+  }
+  for (int i = 1; i < n - 1; ++ i) {
+    auto subAns1 = generateParenthesis(i);
+    auto subAns2 = generateParenthesis(n - 1 - i);
+    for (string &item1 : subAns1) {
+      for (string &item2 : subAns2) {
+        ans.push_back(item1 + "(" + item2 + ")");
+      }
+    }
+  }
+  return ans;
+}
+
+
+// #version3
+// NOTE: take n = 0 as ""
+vector<string> generateParenthesis(int n) {
+  vector<string> ans;
+  if (n == 0) {
+    ans.push_back("");
+    return ans;
+  }
+  for (int i = 0; i < n; ++ i) {
+    auto subAns1 = generateParenthesis(i);
+    auto subAns2 = generateParenthesis(n - 1 - i);
+    for (string &item1 : subAns1) {
+      for (string &item2 : subAns2) {
+        ans.push_back(item1 + "(" + item2 + ")");
+      }
+    }
+  }
+  return ans;
+}
+
+
+// #version4
+// NOTE: most efficient
+void __generateParenthesis(int n, int needRight, string &v, vector<string> &ans) {
+  if (n == 0) {
+    ans.push_back(v + string(needRight, ')'));
+    return;
+  }
+
+  v.push_back('(');
+  __generateParenthesis(n - 1, needRight + 1, v, ans);
+  if (needRight) {
+    v.back() = ')';
+    __generateParenthesis(n, needRight - 1, v, ans);
+  }
+  v.pop_back();
+}
+
+vector<string> generateParenthesis(int n) {
+  vector<string> ans;
+  if (n == 0) return ans;
+  string v;
+  __generateParenthesis(n, 0, v, ans);
+  return ans;
+}
