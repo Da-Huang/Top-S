8978fa49dbf8b59482a8e2968c20ab36fd89aa07
Da Huang <dhuang.cn@gmail.com>
2015-06-21 10:26:36 +0800

    Remove Duplicates from Sorted Array II
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Remove Duplicates from Sorted Array II.cpp
+++ Leet/Remove Duplicates from Sorted Array II.cpp
@@ -1,14 +1,29 @@
-int removeDuplicates(int A[], int n) {
-  int i = 0;
-  int begin = 0;
-  while ( begin < n ) {
-    int end = begin + 1;
-    while ( end < n && A[end] == A[begin] ) end ++;
-    for (int j = 0; j < min(2, end - begin); j ++) {
-      A[i ++] = A[begin + j];
-    }
+// #redo
+int removeDuplicates(vector<int>& nums) {
+  int k = 0;
+  size_t begin = 0;
+  while (begin < nums.size()) {
+    size_t end = begin + 1;
+    while (end < nums.size() && nums[end] == nums[begin]) ++ end;
+    for (size_t i = begin; i < begin + 2 && i < end; ++ i) nums[k ++] = nums[i];
     begin = end;
   }
-  return i;
+  return k;
 }
 
+
+// #version2
+int removeDuplicates(vector<int>& nums) {
+  if (nums.size() == 0) return 0;
+  int k = 1;
+  int count = 1;
+  for (size_t i = 1; i < nums.size(); ++ i) {
+    if (nums[i] == nums[i - 1]) {
+      if (++ count <= 2) nums[k ++] = nums[i];
+    } else {
+      count = 1;
+      nums[k ++] = nums[i];
+    }
+  }
+  return k;
+}
