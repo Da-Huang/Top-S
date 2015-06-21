95ef88dcf215e5ae82193cc77c6dbcb5ef1a3601
Da Huang <dhuang.cn@gmail.com>
2015-06-21 10:08:51 +0800

    Partition List
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Partition List.cpp
+++ Leet/Partition List.cpp
@@ -19,3 +19,23 @@ ListNode *partition(ListNode *head, int x) {
   return NEWHEAD.next;
 }
 
+
+// #redo
+// #version2
+ListNode* partition(ListNode* head, int x) {
+  ListNode HEAD1(0), HEAD2(0);
+  ListNode *tail1 = &HEAD1, *tail2 = &HEAD2;
+  while (head) {
+    if (head->val >= x) {
+      tail2->next = head;
+      tail2 = tail2->next;
+    } else {
+      tail1->next = head;
+      tail1 = tail1->next;
+    }
+    head = head->next;
+  }
+  tail1->next = HEAD2.next;
+  tail2->next = NULL;
+  return HEAD1.next;
+}
