d81c69d03f92728704924022be6891317ab36b94
Da Huang <dhuang.cn@gmail.com>
2015-06-21 13:22:42 +0800

    ZigZag Conversion
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/ZigZag Conversion.cpp
+++ Leet/ZigZag Conversion.cpp
@@ -1,18 +1,15 @@
-string convert(string s, int nRows) {
-  if ( nRows == 1 ) return s;
-  string res;
-  const int N = s.length();
-  for (int k = 0; k < N; k += 2*nRows-2) res.push_back(s[k]);
-  for (int i = 1; i < nRows - 1; i ++) {
-    int t = 1;
-    for (int k = i; k < N; k += 2*nRows-2) {
-      res.push_back(s[k]);
-      int j = (2*nRows-2)*t-k;
-      t += 2;
-      if ( j < N ) res.push_back(s[j]);
-      else break;
+string convert(string s, int numRows) {
+  if (numRows == 1) return s;
+  string ans;
+  const int N = s.size();
+  for (int k = 0; k < N; k += numRows*2-2) ans.push_back(s[k]);
+  for (int i = 1; i < numRows - 1; ++ i) {
+    for (int k = i; k < N; k += numRows*2-2) {
+      ans.push_back(s[k]);
+      int t = k + numRows*2 - 2 - 2 * i;
+      if (t < N) ans.push_back(s[t]);
     }
   }
-  for (int k = nRows-1; k < N; k += 2*nRows-2) res.push_back(s[k]);
-  return res;
+  for (int k = numRows-1; k < N; k += numRows*2-2) ans.push_back(s[k]);
+  return ans;
 }
