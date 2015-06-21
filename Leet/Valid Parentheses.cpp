7bdfd46b84621ef0fe4c29a2f9395c35b7067677
Da Huang <dhuang.cn@gmail.com>
2015-06-21 15:53:30 +0800

    Valid Parentheses
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Valid Parentheses.cpp
+++ Leet/Valid Parentheses.cpp
@@ -1,22 +1,17 @@
 bool isValid(string s) {
-  const int N = s.length();
-  char stk[N]; // stack
-  int stkLen = 0; // length of stack
-  for (int i = 0; i < N; i ++) {
-    if ( s[i] == '(' || s[i] == '[' || s[i] == '{' ) stk[stkLen ++] = s[i];
-    else if ( s[i] == ')' ) {
-      if ( stkLen == 0 || stk[stkLen - 1] != '(' ) return false;
-      stkLen --;
-
-    } else if ( s[i] == ']' ) {
-      if ( stkLen == 0 || stk[stkLen - 1] != '[' ) return false;
-      stkLen --;
-
-    } else { // s[i] == '}'
-      if ( stkLen == 0 || stk[stkLen - 1] != '{' ) return false;
-      stkLen --;
+  stack<char> stk;
+  for (char c : s) {
+    if (c == '(' || c == '[' || c == '{') stk.push(c);
+    else if (c == ')') {
+      if (stk.empty() || stk.top() != '(') return false;
+      stk.pop();
+    } else if (c == ']') {
+      if (stk.empty() || stk.top() != '[') return false;
+      stk.pop();
+    } else { // c == '}'
+      if (stk.empty() || stk.top() != '{') return false;
+      stk.pop();
     }
   }
-  return stkLen == 0;
+  return stk.empty();
 }
-
