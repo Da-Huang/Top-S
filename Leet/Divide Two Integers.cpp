df3b1853c1e62816f7f010d9e3829f36d344df0e
Da Huang <dhuang.cn@gmail.com>
2015-06-21 22:50:27 +0800

    Divide Two Integers
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Divide Two Integers.cpp
+++ Leet/Divide Two Integers.cpp
@@ -1,24 +1,20 @@
 // #redo
 int divide(int dividend, int divisor) {
-  if ( dividend == INT_MIN && divisor == -1 ) return INT_MAX;
-  if ( divisor == 0 ) return dividend > 0 ? INT_MAX : dividend < 0 ? INT_MIN : 0;
-  if ( dividend == 0 ) return 0;
+  if (divisor == 0) return INT_MAX;
+  if (dividend == INT_MIN && divisor == -1) return INT_MAX;
+  if (dividend == 0) return 0;
+  // if (divisor == 1) return dividend; // this line can be removed
 
   bool isNegative = (dividend < 0) ^ (divisor < 0);
-  if ( dividend > 0 ) dividend = -dividend;
-  if ( divisor > 0 ) divisor = -divisor;
-
-  int BOUND = 0;
-  while ( BOUND <= 31 ) {
-    if ( (divisor << (BOUND + 1)) >= 0 || dividend > (divisor << (BOUND + 1)) ) break;
-    BOUND ++;
+  if (dividend > 0) dividend = -dividend;
+  if (divisor > 0) divisor = -divisor;
+  int ans = 0, shift = 1;
+  while (shift < 32 && (divisor << shift) < 0 && dividend <= (divisor << shift)) ++ shift;
+  while (-- shift >= 0) {
+    if (dividend <= (divisor << shift)) {
+      dividend -= divisor << shift;
+      ans |= 1 << shift;
+    }
   }
-  int res = 0;
-  for (int i = BOUND; i >= 0; i --) {
-    if ( dividend <= (divisor << i) ) {
-      dividend -= (divisor << i);
-      res += 1 << i;
-    } 
-  }
-  return isNegative ? -res : res;
+  return isNegative ? -ans : ans;
 }
