ea3ed78c75181ccc4534f596bc5e9540deb44ce4
Da Huang <dhuang.cn@gmail.com>
2015-06-21 13:29:31 +0800

    Palindrome Number
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Palindrome Number.cpp
+++ Leet/Palindrome Number.cpp
@@ -1,14 +1,11 @@
 bool isPalindrome(int x) {
-  if ( x < 0 ) return false;
-  if ( x == 0 ) return true;
-  int high = 1;
-  while ( (x / high) >= 10 ) high *= 10;
-  int low = 1;
-  while ( high > low ) {
-    if ( (x / high) % 10 != (x / low) % 10 ) return false;
-    high /= 10;
-    low *= 10;
+  if (x < 0) return false;
+  int i = 1, j = 1;
+  while (x / j / 10) j *= 10;
+  while (i < j) {
+    if (x / i % 10 != x / j % 10) return false;
+    i *= 10;
+    j /= 10;
   }
   return true;
 }
-
