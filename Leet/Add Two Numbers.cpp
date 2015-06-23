b88f5c9aea6dde82ab59529c3f5f2000a6500829
Da Huang <dhuang.cn@gmail.com>
2015-06-23 15:44:18 +0800

    Add Two Numbers
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Add Two Numbers.cpp
+++ Leet/Add Two Numbers.cpp
@@ -1,30 +1,21 @@
 // #redo
 ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-  int c = 0;
   ListNode HEAD(0);
   ListNode *tail = &HEAD;
-  while (l1 && l2) {
-    int p = l1->val + l2->val + c;
+  int c = 0;
+  while (l1 || l2 || c) {
+    int p = c;
+    if (l1) {
+      p += l1->val;
+      l1 = l1->next;
+    }
+    if (l2) {
+      p += l2->val;
+      l2 = l2->next;
+    }
     tail->next = new ListNode(p % 10);
     tail = tail->next;
     c = p / 10;
-    l1 = l1->next;
-    l2 = l2->next;
   }
-  while (l1) {
-    int p = l1->val + c;
-    tail->next = new ListNode(p % 10);
-    tail = tail->next;
-    c = p / 10;
-    l1 = l1->next;
-  }
-  while (l2) {
-    int p = l2->val + c;
-    tail->next = new ListNode(p % 10);
-    tail = tail->next;
-    c = p / 10;
-    l2 = l2->next;
-  }
-  if (c) tail->next = new ListNode(c);
   return HEAD.next;
 }
