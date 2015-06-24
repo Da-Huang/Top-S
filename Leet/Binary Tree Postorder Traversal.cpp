d10f5dabb7fffda5b61e2e9f65de96c22a91fe45
Da Huang <dhuang.cn@gmail.com>
2015-06-24 15:47:39 +0800

    Binary Tree Postorder Traversal
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Binary Tree Postorder Traversal.cpp
+++ Leet/Binary Tree Postorder Traversal.cpp
@@ -21,44 +21,40 @@ vector<int> postorderTraversal(TreeNode* root) {
 
 
 // #version2
-vector<int> postorderTraversal(TreeNode *root) {
-  vector<int> res;
+vector<int> postorderTraversal(TreeNode* root) {
+  vector<int> ans;
   TreeNode ROOT(0);
   ROOT.left = root;
-  TreeNode *ptr = &ROOT;
-  while ( ptr ) {
-    if ( ptr->left ) {
-      TreeNode *q = ptr->left;
-      while ( q->right && q->right != ptr ) q = q->right;
-
-      if ( q->right == NULL ) {
-        q->right = ptr;
-        ptr = ptr->left;
-
-      } else {
-        q->right = NULL;
-        // reversely collect docs on the right linkedlist of ptr->left
-        TreeNode *head = ptr->left;
-        TreeNode *tail = head;
-        while ( tail->right ) {
+  root = &ROOT;
+  while (root) {
+    TreeNode *lr = root->left;
+    if (lr) {
+      while (lr->right && lr->right != root) lr = lr->right;
+      if (lr->right) {
+        lr->right = NULL;
+        TreeNode *head = root->left, *tail = root->left;
+        while (tail->right) {
           TreeNode *node = tail->right;
           tail->right = node->right;
           node->right = head;
           head = node;
         }
         tail = head;
-        res.push_back(tail->val);
-        while ( tail->right ) {
+        ans.push_back(tail->val);
+        while (tail->right) {
           TreeNode *node = tail->right;
-          res.push_back(node->val);
+          ans.push_back(node->val);
           tail->right = node->right;
           node->right = head;
           head = node;
         }
-        ptr = ptr->right;
+      } else {
+        lr->right = root;
+        root = root->left;
+        continue;
       }
-    } else ptr = ptr->right;
+    }
+    root = root->right;
   }
-  return res;
+  return ans;
 }
-
