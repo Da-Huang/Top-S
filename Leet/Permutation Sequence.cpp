facbfe5f8524904351a57302cb8c54851f1f9a6a
Da Huang <dhuang.cn@gmail.com>
2015-07-06 22:25:09 +0800

    Permutation Sequence
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Permutation Sequence.cpp
+++ Leet/Permutation Sequence.cpp
@@ -1,33 +1,24 @@
+// #redo
 string getPermutation(int n, int k) {
-  int NUMS[n + 1];
-  NUMS[0] = 1;
-  for (int i = 1; i <= n; i ++)
-    NUMS[i] = NUMS[i - 1] * i;
-  // NUMS[n] == n!
-  k = (k - 1) % NUMS[n] + 1;
-
-  string res;
-  vector<bool> selected(n + 1, false);
-  for (int i = n - 1; i >= 0; i --) {
-    int j = 1;
-    while ( j < n ) {
-      if ( k <= NUMS[i] * j ) {
-        break;
-      }
-      j ++;
-    }
-    k -= NUMS[i] * (j - 1);
-    int count = 0;
-    for (int t = 1; t <= n; t ++) {
-      if ( !selected[t] ) {
-        count ++;
-        if ( count == j ) {
-          selected[t] = true;
-          res += to_string(t);
+  int T = 1;
+  for (int i = 1; i <= n; ++ i) T *= i;
+  k = (k - 1) % T;
+  vector<bool> visited(n, false);
+  string ans;
+  for (int i = n - 1; i >= 0; -- i) {
+    T /= i + 1;
+    int c = k / T;
+    for (int j = 0; j < n; ++ j) {
+      if (!visited[j]) {
+        if (c == 0) {
+          ans.push_back(j + '1');
+          visited[j] = true;
           break;
         }
+        -- c;
       }
     }
+    k %= T;
   }
-  return res;
+  return ans;
 }
