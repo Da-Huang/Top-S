7ab6099793da3f5416c4da4cf8e8b7b0049e6e11
Da Huang <dhuang.cn@gmail.com>
2015-07-06 22:23:15 +0800

    N-Queens II
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/N-Queens II.cpp
+++ Leet/N-Queens II.cpp
@@ -18,3 +18,29 @@ int totalNQueens(int n) {
   if (n == 0) return 0; // NOTE: should be clarified
   return __totalNQueens(n, 0, 0, 0, 0);
 }
+
+
+// #version2
+int __totalNQueens(int n, int begin, int x[]) {
+  if (begin == n) return 1;
+  vector<bool> able(n, true);
+  for (int i = 0; i < begin; ++ i) {
+    able[x[i]] = false;
+    if (x[i] + (begin - i) < n) able[x[i] + (begin - i)] = false;
+    if (x[i] - (begin - i) >= 0) able[x[i] - (begin - i)] = false;
+  }
+  int ans = 0;
+  for (int i = 0; i < n; ++ i) {
+    if (able[i]) {
+      x[begin] = i;
+      ans += __totalNQueens(n, begin + 1, x);
+    }
+  }
+  return ans;
+}
+
+int totalNQueens(int n) {
+  if (n == 0) return 0;
+  int x[n];
+  return __totalNQueens(n, 0, x);
+}
