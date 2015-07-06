b8fcc18c6492fb4a3b46de9d4578655f24fe6aba
Da Huang <dhuang.cn@gmail.com>
2015-07-06 22:24:05 +0800

    Sqrt(x)
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Sqrt(x).cpp
+++ Leet/Sqrt(x).cpp
@@ -1,15 +1,12 @@
-// Redo
-int sqrt(int x) {
-  if ( x == 0 ) return 0;
-  int last = x;
-  int first = 1;
-  while ( first < last ) {
-    const int mid = first + (last - first) / 2;
-    if ( mid > x / mid ) last = mid - 1;
-    else if ( mid < x / mid ) first = mid + 1;
+// #redo
+int mySqrt(int x) {
+  if (x <= 1) return x;
+  int first = 1, last = x;
+  while (first < last) {
+    int mid = first + (last - first) / 2;
+    if (mid > x / mid) last = mid;
+    else if (mid < x / mid) first = mid + 1;
     else return mid;
   }
-  if ( first > x / first ) return first - 1;
-  else return first;
+  return first - 1;
 }
-
