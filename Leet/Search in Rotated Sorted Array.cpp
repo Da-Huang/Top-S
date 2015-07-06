7f4f46bba76b9999d05411793506cdc6db9795ca
Da Huang <dhuang.cn@gmail.com>
2015-07-06 22:18:09 +0800

    Search in Rotated Sorted Array
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Search in Rotated Sorted Array.cpp
+++ Leet/Search in Rotated Sorted Array.cpp
@@ -1,24 +1,23 @@
-int findBound(int A[], int first, int last) {
-  while ( first < last ) {
-    const int mid = (first + last) / 2;
-    if ( A[mid] >= A[0] ) first = mid + 1;
-    else last = mid - 1;
-  }
-  if ( A[first] < A[0] ) return first;
-  else return first + 1;
-}
-
-int binarySearch(int A[], int first, int last, int target) {
-  while ( first <= last ) {
-    const int mid = (first + last) / 2;
-    if ( A[mid] > target ) last = mid - 1;
-    else if ( A[mid] < target ) first = mid + 1;
+// #redo
+int __search(vector<int> &nums, int target, int first, int last) {
+  while (first < last) {
+    int mid = first + (last - first) / 2;
+    if (nums[mid] > target) last = mid;
+    else if (nums[mid] < target) first = mid + 1;
     else return mid;
   }
   return -1;
 }
 
-int search(int A[], int n, int target) {
-  int bound = findBound(A, 0, n - 1);
-  return max(binarySearch(A, 0, bound - 1, target), binarySearch(A, bound, n - 1, target));
+int search(vector<int>& nums, int target) {
+  int first = 0, last = (int) nums.size();
+  while (first < last) {
+    int mid = first + (last - first) / 2;
+    if (nums[mid] < nums[first]) last = mid;
+    else {
+      first = mid + 1;
+      if (first < last && nums[first] < nums[mid]) break;
+    }
+  }
+  return max(__search(nums, target, 0, first), __search(nums, target, first, (int) nums.size()));
 }
