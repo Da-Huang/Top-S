1696421a96779d608138b046c9efdb3417eac2a4
Da Huang <dhuang.cn@gmail.com>
2015-07-06 22:24:40 +0800

    Unique Paths II
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Unique Paths II.cpp
+++ Leet/Unique Paths II.cpp
@@ -1,16 +1,18 @@
-int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
+// #redo
+int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
   const int M = obstacleGrid.size();
-  if ( M == 0 ) return 0;
+  if (M == 0) return 0;
   const int N = obstacleGrid[0].size();
-  if ( N == 0 ) return 0;
-  int res[M][N];
-  res[0][0] = obstacleGrid[0][0] ? 0 : 1;
-  for (int i = 1; i < M; i ++) res[i][0] = obstacleGrid[i][0] ? 0 : res[i - 1][0];
-  for (int j = 1; j < N; j ++) res[0][j] = obstacleGrid[0][j] ? 0 : res[0][j - 1];
-  for (int i = 1; i < M; i ++) {
-    for (int j = 1; j < N; j ++) {
-      res[i][j] = obstacleGrid[i][j] ? 0 : res[i - 1][j] + res[i][j - 1];
+  if (N == 0) return 0;
+
+  int ans[M][N];
+  ans[0][0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+  for (int j = 1; j < N; ++ j) ans[0][j] = obstacleGrid[0][j] == 1 ? 0 : ans[0][j-1];
+  for (int i = 1; i < M; ++ i) {
+    ans[i][0] = obstacleGrid[i][0] == 1 ? 0 : ans[i-1][0];
+    for (int j = 1; j < N; ++ j) {
+      ans[i][j] = obstacleGrid[i][j] == 1 ? 0 : ans[i-1][j] + ans[i][j-1];
     }
   }
-  return res[M - 1][N - 1];
+  return ans[M-1][N-1];
 }
