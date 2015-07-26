b1346aed9c4508759e006c1bbd7fdcf9bca28b30
Da Huang <dhuang.cn@gmail.com>
2015-07-26 22:53:28 +0800

    Lowest Common Ancestor of a Binary Tree
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

---
+++ Leet/Lowest Common Ancestor of a Binary Tree.cpp
@@ -0,0 +1,9 @@
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+  if (root == NULL) return NULL;
+  if (root == p || root == q) return root;
+  TreeNode *left = lowestCommonAncestor(root->left, p, q);
+  TreeNode *right = lowestCommonAncestor(root->right, p, q);
+  if (left == NULL) return right;
+  if (right == NULL) return left;
+  return root;
+}
