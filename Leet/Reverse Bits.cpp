4c5e3cd281de024a06aff246247717edb8c6b4d9
Da Huang <dhuang.cn@gmail.com>
2015-08-01 20:09:52 +0800

    Reverse Bits
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Reverse Bits.cpp
+++ Leet/Reverse Bits.cpp
@@ -6,3 +6,12 @@ uint32_t reverseBits(uint32_t n) {
   }
   return ans;
 }
+
+// #version2
+uint32_t reverseBits(uint32_t n) {
+  n = ((n & 0x55555555) << 1) | ((n >> 1) & 0x55555555);
+  n = ((n & 0x33333333) << 2) | ((n >> 2) & 0x33333333);
+  n = ((n & 0x0F0F0F0F) << 4) | ((n >> 4) & 0x0F0F0F0F);
+  n = ((n & 0x00FF00FF) << 8) | ((n >> 8) & 0x00FF00FF);
+  return ((n & 0x0000FFFF) << 16) | ((n >> 16) & 0x0000FFFF);
+}
