0b95ab3a6306776f619f72d9cc5a3f049119aa69
Da Huang <dhuang.cn@gmail.com>
2015-08-08 16:51:10 +0800

    Palindrome Linked List
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Palindrome Linked List.cpp
+++ Leet/Palindrome Linked List.cpp
@@ -29,3 +29,42 @@ bool isPalindrome(ListNode* head) {
   half->next = __reverse(half->next);
   return ans;
 }
+
+// #version2
+ListNode *__reverse(ListNode *head) { // head must not be nullptr
+  ListNode *tail = head;
+  while (tail->next) {
+    ListNode *node = tail->next;
+    tail->next = node->next;
+    node->next = head;
+    head = node;
+  }
+  return head;
+}
+
+bool isPalindrome(ListNode* head) {
+  if (head == nullptr || head->next == nullptr) return true; 
+  // At least, two nodes
+  ListNode* half = head, *p = head->next;
+  while (p) {
+    p = p->next;
+    if (p) {
+      p = p->next;
+      half = half->next;
+    }
+  }
+  ListNode *head2 = half->next; // head2 != nullptr
+  head2 = __reverse(head2);
+  ListNode *ptr1 = head, *ptr2 = head2;
+  bool ans = true;
+  while (ptr2) {
+    if (ptr1->val != ptr2->val) {
+      ans = false;
+      break;
+    }
+    ptr1 = ptr1->next;
+    ptr2 = ptr2->next;
+  }
+  half->next = __reverse(head2);
+  return ans;
+}
