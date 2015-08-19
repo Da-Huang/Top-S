12ec774be8e47dfb2a20c5dbfee4c9f5246922a7
Da Huang <dhuang.cn@gmail.com>
2015-08-20 00:25:13 +0800

    Ugly Number
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

---
+++ Leet/Ugly Number.cpp
@@ -0,0 +1,10 @@
+bool isUgly(int num) {
+  if (num < 1) return false;
+  while (num > 1) {
+    if (num % 2 == 0) num /= 2;
+    else if (num % 3 == 0) num /= 3;
+    else if (num % 5 == 0) num /= 5;
+    else return false;
+  }
+  return true;
+}
