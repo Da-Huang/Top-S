769ed6710bb9c0f7e2199c91e86cacbe32dd4cde
Da Huang <dhuang.cn@gmail.com>
2015-08-22 20:44:56 +0800

    Convert Sorted Array to Binary Search Tree With Minimal Height
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

---
+++ Lint/Convert Sorted Array to Binary Search Tree With Minimal Height.cpp
@@ -0,0 +1,13 @@
+// #redo
+TreeNode *__sortedArrayToBST(vector<int> &A, int first, int last) {
+  if (first > last) return nullptr;
+  int mid = first + (last - first) / 2;
+  TreeNode *root = new TreeNode(A[mid]);
+  root->left = __sortedArrayToBST(A, first, mid - 1);
+  root->right = __sortedArrayToBST(A, mid + 1, last);
+  return root;
+}
+ 
+TreeNode *sortedArrayToBST(vector<int> &A) {
+  return __sortedArrayToBST(A, 0, (int)A.size() - 1);
+}
