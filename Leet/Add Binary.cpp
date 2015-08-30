4c60d8040efcf638f62486929f6f300c0beb2640
Da Huang <dhuang.cn@gmail.com>
2015-08-30 13:31:17 +0800

    little changes
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Add Binary.cpp
+++ Leet/Add Binary.cpp
@@ -7,8 +7,8 @@ string addBinary(string a, string b) {
   size_t i = 0, j = 0;
   while (i < a.size() || j < b.size() || c) {
     int p = c;
-    if (i < a.size()) p += a[i ++] - '0';
-    if (j < b.size()) p += b[j ++] - '0';
+    if (i < a.size()) p += a[i++] - '0';
+    if (j < b.size()) p += b[j++] - '0';
     ans.push_back((p & 1) + '0');
     c = p >> 1;
   }
