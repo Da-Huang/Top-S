6f65b7f195daad7213124cc2b4559e5b0d751036
Da Huang <dhuang.cn@gmail.com>
2015-08-31 10:30:17 +0800

    Segment Tree Query II
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Lint/Segment Tree Query II.cpp
+++ Lint/Segment Tree Query II.cpp
@@ -1,8 +1,7 @@
 // #redo
 int query(SegmentTreeNode *root, int start, int end) {
-  if (root == nullptr || end < root->start || start > root->end) return 0;
+  if (root == nullptr) return 0;
+  if (start > root->end || end < root->start) return 0;
   if (start <= root->start && root->end <= end) return root->count;
-  if (end <= root->left->end) return query(root->left, start, end);
-  if (root->right->start <= start) return query(root->right, start, end);
   return query(root->left, start, end) + query(root->right, start, end);
 }
