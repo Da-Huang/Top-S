50d6bd731acad7feb06af305b6fa0a0765aa16d8
Da Huang <dhuang.cn@gmail.com>
2015-09-11 11:00:07 +0800

    Basic Calculator II
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Basic Calculator II.cpp
+++ Leet/Basic Calculator II.cpp
@@ -8,31 +8,99 @@ int calculate(string s) {
   stack<char> ops;
   size_t i = 0;
   while (i <= s.size()) {
-    if (i == s.size() || s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/') {
+    if (i == s.size() || s[i] == '+' || s[i] == '-' || s[i] == '*' ||
+        s[i] == '/') {
       char op = i == s.size() ? '#' : s[i];
       while (!ops.empty() && priority[ops.top()] >= priority[op]) {
         long long v2 = stk.top();
         stk.pop();
-        switch(ops.top()) {
-          case '+': stk.top() += v2; break;
-          case '-': stk.top() -= v2; break;
-          case '*': stk.top() *= v2; break;
-          case '/': stk.top() /= v2; break;
+        switch (ops.top()) {
+          case '+':
+            stk.top() += v2;
+            break;
+          case '-':
+            stk.top() -= v2;
+            break;
+          case '*':
+            stk.top() *= v2;
+            break;
+          case '/':
+            stk.top() /= v2;
+            break;
         }
         ops.pop();
       }
       ops.push(op);
-      ++ i;
+      ++i;
 
     } else if (isdigit(s[i])) {
       size_t j = i + 1;
-      while (j < s.size() && isdigit(s[j])) ++ j;
+      while (j < s.size() && isdigit(s[j])) ++j;
       stk.push(stoll(s.substr(i, j - i)));
       i = j;
 
     } else if (s[i] == ' ') {
-      ++ i;
+      ++i;
     }
   }
   return stk.top();
+}
+
+// #version2
+void __pop_ops(stack<char> &ops, stack<long long> &operands,
+               const string &stops) {
+  while (!ops.empty()) {
+    size_t i = 0;
+    for (; i < stops.size(); ++i) {
+      if (ops.top() == stops[i]) break;
+    }
+    if (i < stops.size()) break;
+    long long num = operands.top();
+    operands.pop();
+    switch (ops.top()) {
+      case '+':
+        operands.top() += num;
+        break;
+      case '-':
+        operands.top() -= num;
+        break;
+      case '*':
+        operands.top() *= num;
+        break;
+      case '/':
+        operands.top() /= num;
+        break;
+    }
+    ops.pop();
+  }
+}
+
+int calculate(string s) {
+  stack<long long> operands;
+  stack<char> ops;
+  size_t i = 0;
+  while (i < s.size()) {
+    switch (s[i]) {
+      case '+':
+      case '-':
+        __pop_ops(ops, operands, "");
+        ops.push(s[i]);
+        break;
+      case '*':
+      case '/':
+        __pop_ops(ops, operands, "+-");
+        ops.push(s[i]);
+        break;
+      case ' ':
+        break;
+      default:
+        size_t j = i + 1;
+        while (j < s.size() && isdigit(s[j])) ++j;
+        operands.push(stoll(s.substr(i, j - i)));
+        i = j - 1;
+    }
+    ++i;
+  }
+  __pop_ops(ops, operands, "");
+  return operands.top();
 }
