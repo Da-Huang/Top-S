2d951864222f20516cef7b366e8218a0075656c6
Da Huang <dhuang.cn@gmail.com>
2015-11-06 10:58:21 +0800

    Copy List with Random Pointer
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- Leet/Copy List with Random Pointer.cpp
+++ Leet/Copy List with Random Pointer.cpp
@@ -1,15 +1,39 @@
-RandomListNode *copyRandomList(RandomListNode *head) {
+RandomListNode* copyRandomList(RandomListNode* head) {
   unordered_map<RandomListNode*, RandomListNode*> MAP;
   RandomListNode HEAD(0);
-  RandomListNode *tail = &HEAD;
-  for (RandomListNode *ptr = head; ptr; ptr = ptr->next) {
+  RandomListNode* tail = &HEAD;
+  for (RandomListNode* ptr = head; ptr; ptr = ptr->next) {
     tail->next = MAP[ptr] = new RandomListNode(ptr->label);
     tail = tail->next;
   }
-  for (RandomListNode *ptr = head; ptr; ptr = ptr->next) {
+  for (RandomListNode* ptr = head; ptr; ptr = ptr->next) {
     if (ptr->random) {
       MAP[ptr]->random = MAP[ptr->random];
     }
+  }
+  return HEAD.next;
+}
+
+// #version2
+RandomListNode* copyRandomList(RandomListNode* head) {
+  for (RandomListNode* ptr = head; ptr; ptr = ptr->next) {
+    RandomListNode* node = new RandomListNode(ptr->label);
+    node->next = ptr->next;
+    ptr->next = node;
+    ptr = node;
+  }
+  for (RandomListNode* ptr = head; ptr; ptr = ptr->next) {
+    RandomListNode* node = ptr->next;
+    node->random = ptr->random ? ptr->random->next : nullptr;
+    ptr = node;
+  }
+  RandomListNode HEAD(0);
+  RandomListNode* tail = &HEAD;
+  for (RandomListNode* ptr = head; ptr; ptr = ptr->next) {
+    RandomListNode* node = ptr->next;
+    ptr->next = node->next;
+    tail->next = node;
+    tail = node;
   }
   return HEAD.next;
 }
